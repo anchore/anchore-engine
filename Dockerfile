8c28138b7f47d7f4d86308130622e5b3042ab247
Daniel Nurmi <nurmi@anchore.com>
2017-09-07 13:01:14 -0700

    updates to build and deploy scripts
    Signed-off-by: Daniel Nurmi <nurmi@anchore.com>

--- Dockerfile
+++ Dockerfile
@@ -1,7 +1,7 @@
 FROM centos:latest
 
 ENV LANG=en_US.UTF-8
-EXPOSE 80
+EXPOSE 8228 8338
 
 RUN yum -y update && yum -y install epel-release && yum -y install skopeo python-pip dpkg gcc python-devel openssl-devel && yum clean all -y
 RUN pip install --upgrade pip && pip install --upgrade setuptools 
@@ -11,21 +11,5 @@ COPY ./anchore-cli /root/anchore-cli
 COPY ./anchore /root/anchore
 
 RUN cd /root/anchore-engine/ && pip install --upgrade . && cd /root/anchore/ && pip install --upgrade . && cd /root/anchore-cli/ && pip install --upgrade .
-# RUN pip uninstall -y enum34; pip uninstall -y enum; pip install enum==0.4.6
 
 CMD /usr/bin/anchore-engine
-
-#
-# TO USE
-#
-# docker run --name some-postgres -e POSTGRES_PASSWORD=mysecretpassword -p 5432:5432 -d postgres
-# emacs -nw /root/anchore-engine-configfiles/config.yaml
-# docker run -v /var/run/docker.sock:/var/run/docker.sock -v /root/anchore-engine-configfiles/:/config/ -p 8080:80 -d --name anchore-engine anchore-engine
-#
-# curl -XPOST -u admin:foobar http://localhost:8080/v1/images -d'{"tag":"alpine"}'
-# curl -XPOST -u admin:foobar http://localhost:8080/v1/policies -d"@/root/somebundle.json"
-# curl -u admin:foobar http://localhost:8080/v1/images
-# curl -u admin:foobar http://localhost:8080/v1/images/<img_url_id>
-# curl -u admin:foobar http://localhost:8080/v1/images/<img_url_id>/check
-# ....
-#
--- build_docker_image.sh
+++ build_docker_image.sh
@@ -1,9 +1,9 @@
 #!/bin/bash -x
 
-if [ -z "$ANCHORESRCHOME" ]; then
-    echo "set ANCHORESRCHOME to directory that contains anchore-engine, anchore, and anchore-cli"
-    exit 1
-fi
+#if [ -z "$ANCHORESRCHOME" ]; then
+#    echo "set ANCHORESRCHOME to directory that contains anchore-engine, anchore, and anchore-cli"
+#    exit 1
+#fi
 
 if [ "$1" == "use-cache" ]
 then
@@ -25,12 +25,12 @@ rm -rf /tmp/anchore-engine-build/anchore
 cp -a Dockerfile /tmp/anchore-engine-build
 cd /tmp/anchore-engine-build
 
-#git clone git@github.com:anchore/anchore-engine.git
-#git clone git@github.com:anchore/anchore-cli.git
-#git clone git@github.com:anchore/anchore.git
+git clone git@github.com:anchore/anchore-engine.git
+git clone git@github.com:anchore/anchore-cli.git
+git clone git@github.com:anchore/anchore.git
 
-rsync --delete -azP /${ANCHORESRCHOME}/anchore-engine/ /tmp/anchore-engine-build/anchore-engine/
-rsync --delete -azP /${ANCHORESRCHOME}/anchore-cli/ /tmp/anchore-engine-build/anchore-cli/
-rsync --delete -azP /${ANCHORESRCHOME}/anchore/ /tmp/anchore-engine-build/anchore/
+#rsync --delete -azP /${ANCHORESRCHOME}/anchore-engine/ /tmp/anchore-engine-build/anchore-engine/
+#rsync --delete -azP /${ANCHORESRCHOME}/anchore-cli/ /tmp/anchore-engine-build/anchore-cli/
+#rsync --delete -azP /${ANCHORESRCHOME}/anchore/ /tmp/anchore-engine-build/anchore/
 
 cd /tmp/anchore-engine-build && docker build -t anchore-engine:latest ${cache_directive} . && docker tag anchore-engine:latest anchore/anchore-engine:latest
