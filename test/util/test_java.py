84f862e668cdebe7e183bc5b310d77a1125e28b2
Matt Sicker <boards@gmail.com>
2018-10-12 11:38:28 -0500

    Use pytest for test
    Signed-off-by: Matt Sicker <boards@gmail.com>

--- test/util/test_java.py
+++ test/util/test_java.py
@@ -1,18 +1,16 @@
-import unittest
 import anchore_engine.util.java as java_util
 
-class TestJava(unittest.TestCase):
-    
-    def test_parse_properties(self):
-        properties = """
-        foo = bar
-        # commented = yes
-        test = true
-        """
-        
-        props = java_util.parse_properties(properties.splitlines())
+def test_parse_properties():
+    properties = """
+    prop1=val1
+    prop2 = val2
+    #prop3 = ignored due to comments
+      prop4 = val4
+    """
 
-        self.assertEqual('bar', props['foo'])
-        self.assertEqual('true', props['test'])
-        self.assertNotIn('commented', props)
+    props = java_util.parse_properties(properties.splitlines())
 
+    assert props['prop1'] == 'val1'
+    assert props['prop2'] == 'val2'
+    assert 'prop3' not in props
+    assert props['prop4'] == 'val4'
