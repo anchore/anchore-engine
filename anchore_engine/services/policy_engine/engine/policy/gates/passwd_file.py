fac9771dc49c5e3f49496afb97d78c759a2dc670
Zach Hill <zach@anchore.com>
2019-04-15 15:53:08 -0700

    Fixes bug in passwd_file gate's context setup that was parsing entries incorrectly.
    Signed-off-by: Zach Hill <zach@anchore.com>

--- anchore_engine/services/policy_engine/engine/policy/gates/passwd_file.py
+++ anchore_engine/services/policy_engine/engine/policy/gates/passwd_file.py
@@ -1,3 +1,4 @@
+from anchore_engine.utils import ensure_str, ensure_bytes
 from anchore_engine.services.policy_engine.engine.policy.gate import Gate, BaseTrigger
 from anchore_engine.services.policy_engine.engine.policy.params import CommaDelimitedNumberListParameter, CommaDelimitedStringListParameter, TriggerParameter, TypeValidator
 from anchore_engine.db import AnalysisArtifact
@@ -156,7 +157,7 @@ class FileparsePasswordGate(Gate):
         if content_matches:
             try:
                 pentries = {}
-                for line in str(content_matches.binary_value).splitlines():
+                for line in ensure_str(content_matches.binary_value).splitlines():
                     line = line.strip()
                     pentry = line.split(':')
                     pentries[pentry[0]] = pentry[1:]
