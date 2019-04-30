91cd587fb5819672b4321e6133575475b9c1be68
Zach Hill <zach@anchore.com>
2019-04-30 11:30:38 -0700

    Comment analysis archive rule history since not yet fully supported.
    Signed-off-by: Zach Hill <zach@anchore.com>

--- anchore_engine/services/apiext/api/controllers/archive.py
+++ anchore_engine/services/apiext/api/controllers/archive.py
@@ -133,31 +133,31 @@ def get_analysis_archive_rule(ruleId):
         return handle_proxy_response(ex)
 
 
-@authorizer.requires([ActionBoundPermission(domain=RequestingAccountValue())])
-def get_analysis_archive_rule_history(ruleId):
-    """
-
-    GET /archives/rules/{ruleId}/history
-
-    :param ruleId:
-    :return: list of events for the rule
-    """
-    client = internal_client_for(CatalogClient, ApiRequestContextProxy.namespace())
-    try:
-        resp1 = handle_proxy_response(client.get_analysis_archive_rule_history(ruleId))
-        if resp1[1] == 404 and ApiRequestContextProxy.namespace() != ADMIN_ACCOUNT_NAME:
-            # Yes, this is a bit ugly
-            # Get the rule, check if a global rule and adjust error code appropriately
-            try:
-                c2 = internal_client_for(CatalogClient, ADMIN_ACCOUNT_NAME)
-                r2 = handle_proxy_response(c2.get_analysis_archive_rule(ruleId))
-                if r2 and r2[1] == 200 and r2[0].get('system_global', False):
-                    return make_response_error('Non-admins cannot modify/delete system global rules', in_httpcode=403), 403
-            except Exception as ex:
-                pass
-        return resp1
-    except Exception as ex:
-        return handle_proxy_response(ex)
+# @authorizer.requires([ActionBoundPermission(domain=RequestingAccountValue())])
+# def get_analysis_archive_rule_history(ruleId):
+#     """
+#
+#     GET /archives/rules/{ruleId}/history
+#
+#     :param ruleId:
+#     :return: list of events for the rule
+#     """
+#     client = internal_client_for(CatalogClient, ApiRequestContextProxy.namespace())
+#     try:
+#         resp1 = handle_proxy_response(client.get_analysis_archive_rule_history(ruleId))
+#         if resp1[1] == 404 and ApiRequestContextProxy.namespace() != ADMIN_ACCOUNT_NAME:
+#             # Yes, this is a bit ugly
+#             # Get the rule, check if a global rule and adjust error code appropriately
+#             try:
+#                 c2 = internal_client_for(CatalogClient, ADMIN_ACCOUNT_NAME)
+#                 r2 = handle_proxy_response(c2.get_analysis_archive_rule(ruleId))
+#                 if r2 and r2[1] == 200 and r2[0].get('system_global', False):
+#                     return make_response_error('Non-admins cannot modify/delete system global rules', in_httpcode=403), 403
+#             except Exception as ex:
+#                 pass
+#         return resp1
+#     except Exception as ex:
+#         return handle_proxy_response(ex)
 
 
 @authorizer.requires([ActionBoundPermission(domain=RequestingAccountValue())])
