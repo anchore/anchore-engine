6a174c85227f1959e8fb10c6d5aeda2dda06eb1a
Mattia Pagnozzi <mattia.pagnozzi@gmail.com>
2019-05-14 19:13:24 +0200

    Don't return an iterator from list_accounts() Since we may want to loop over the result multiple times, don't return an iterator in list_accounts() but wrap the result in a list.
    Signed-off-by: Mattia Pagnozzi <mattia.pagnozzi@gmail.com>

--- anchore_engine/subsys/identities.py
+++ anchore_engine/subsys/identities.py
@@ -188,8 +188,8 @@ class IdentityManager(object):
         return account
 
     def list_accounts(self, with_state=None, include_service=False):
-        accounts = filter(lambda x: (include_service or (x['type'] != AccountTypes.service)),
-                          db_accounts.get_all(with_state=with_state, session=self.session))
+        accounts = list(filter(lambda x: (include_service or (x['type'] != AccountTypes.service)),
+                          db_accounts.get_all(with_state=with_state, session=self.session)))
         return accounts
 
     def get_account(self, accountname):
