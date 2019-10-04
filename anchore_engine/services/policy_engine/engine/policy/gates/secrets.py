cccc42fd65705b4011123fffc03e0ec24d5ef85c
Daniel Nurmi <nurmi@anchore.com>
2019-10-04 14:25:14 -0700

    add a new parameter to secret_search gate, which allows the bundle to specify whether to trigger if a match is found (default) or is not found (new behavior).  Fixes #264.
    Signed-off-by: Daniel Nurmi <nurmi@anchore.com>

--- anchore_engine/services/policy_engine/engine/policy/gates/secrets.py
+++ anchore_engine/services/policy_engine/engine/policy/gates/secrets.py
@@ -3,7 +3,7 @@ import base64
 from anchore_engine.utils import ensure_bytes, ensure_str
 from anchore_engine.services.policy_engine.engine.policy.gate import Gate, BaseTrigger
 from anchore_engine.services.policy_engine.engine.logs import get_logger
-from anchore_engine.services.policy_engine.engine.policy.params import TypeValidator, TriggerParameter
+from anchore_engine.services.policy_engine.engine.policy.params import TypeValidator, TriggerParameter, EnumStringParameter
 from anchore_engine.db import AnalysisArtifact
 log = get_logger()
 
@@ -13,14 +13,17 @@ default_included_regex_names = ["AWS_ACCESS_KEY", "AWS_SECRET_KEY", "PRIV_KEY", 
 
 class SecretContentChecksTrigger(BaseTrigger):
     __trigger_name__ = 'content_regex_checks'
-    __description__ = 'Triggers if the content search analyzer has found any matches with the configured and named regexes. Matches are filtered by the content_regex_name and filename_regex if they are set. The content_regex_name shoud be a value from the "secret_search" section of the analyzer_config.yaml.'
+    __description__ = 'Triggers if the secret content search analyzer has found any matches with the configured and named regexes. Checks can be configured to trigger if a match is found or is not found (selected using match_type parameter).  Matches are filtered by the content_regex_name and filename_regex if they are set. The content_regex_name shoud be a value from the "secret_search" section of the analyzer_config.yaml.'
 
-    secret_contentregexp = TriggerParameter(name='content_regex_name', validator=TypeValidator('string'), example_str=default_included_regex_names[0], description='Name of content regexps configured in the analyzer that should trigger if found in the image, instead of triggering for any match. Names available by default are: {}.'.format(default_included_regex_names))
-    name_regexps = TriggerParameter(name='filename_regex', validator=TypeValidator('string'), example_str='/etc/.*', description='Regexp to filter the content matched files by.')
-
+    secret_contentregexp = TriggerParameter(name='content_regex_name', validator=TypeValidator('string'), example_str=default_included_regex_names[0], description='Name of content regexps configured in the analyzer that match if found in the image, instead of matching all. Names available by default are: {}.'.format(default_included_regex_names), sort_order=1)
+    name_regexps = TriggerParameter(name='filename_regex', validator=TypeValidator('string'), example_str='/etc/.*', description='Regexp to filter the content matched files by.', sort_order=2)
+    match_type = EnumStringParameter(name='match_type', enum_values=['notfound', 'found'], example_str='found', description='Set to define the type of match - trigger if match is found (default) or not found.', is_required=False, sort_order=3)
+    
     def evaluate(self, image_obj, context):
         match_filter = self.secret_contentregexp.value(default_if_none=[])
-        name_re = re.compile(self.name_regexps.value()) if self.name_regexps.value() else None
+        name_filter = self.name_regexps.value()
+        name_re = re.compile(name_filter) if self.name_regexps.value() else None
+        match_type = self.match_type.value(default_if_none="found")
 
         if match_filter:
             matches = [base64.b64encode(ensure_bytes(x)) for x in match_filter]
@@ -29,6 +32,7 @@ class SecretContentChecksTrigger(BaseTrigger):
             matches = []
             matches_decoded = []
 
+        found=False
         for thefile, regexps in list(context.data.get('secret_content_regexp', {}).items()):
             thefile = ensure_str(thefile)
 
@@ -46,16 +50,28 @@ class SecretContentChecksTrigger(BaseTrigger):
                         theregexp = decoded_regexp
 
                     if not matches:
-                        self._fire(msg='Secret search analyzer found regexp match in container: file={} regexp={}'.format(thefile, decoded_regexp))
+                        found=True
                     elif regexp in matches or theregexp in matches_decoded:
-                        self._fire(msg='Secret search analyzer found regexp match in container: file={} regexp={}'.format(thefile, decoded_regexp))
+                        found=True
                     elif regexp_name and regexp_name in matches_decoded:
-                        self._fire(msg='Secret search analyzer found regexp match in container: file={} regexp={}'.format(thefile, decoded_regexp))
+                        found=True
+                    if found and match_type=='found':
+                        self._fire(msg='Secret content search analyzer found regexp match in container: file={} regexp={}'.format(thefile, decoded_regexp))
+
+        if not found and match_type=='notfound':
+            f_filter = name_filter
+            if not f_filter:
+                f_filter = '*'
+
+            m_filter = match_filter
+            if not m_filter:
+                m_filter = 'all'
+            self._fire(msg='Secret content search analyzer did not find regexp match in container: filename_regex={} content_regex_name={}'.format(f_filter, m_filter))
 
 
 class SecretCheckGate(Gate):
     __gate_name__ = 'secret_scans'
-    __description__ = 'Checks for secrets found in the image using configured regexes found in the "secret_search" section of analyzer_config.yaml.'
+    __description__ = 'Checks for secrets and content found in the image using configured regexes found in the "secret_search" section of analyzer_config.yaml.'
     __triggers__ = [
         SecretContentChecksTrigger
     ]
