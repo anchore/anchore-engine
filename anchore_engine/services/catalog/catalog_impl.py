613ffbbfe4eaae9d717f90785bc0a74980808f51
Swathi Gangisetty <swathi@anchore.com>
2019-12-03 18:09:20 -0800

    Include status in the policy evaluation update event
    Signed-off-by: Swathi Gangisetty <swathi@anchore.com>

--- anchore_engine/services/catalog/catalog_impl.py
+++ anchore_engine/services/catalog/catalog_impl.py
@@ -1269,9 +1269,13 @@ def perform_policy_evaluation(userId, imageDigest, dbsession, evaltag=None, poli
         except Exception as err:
             raise err
         curr_final_action = curr_evaluation_result.get('final_action', '').upper()
-        # TODO hack! Include image digest, needed for the downstream notifications handler
-        if 'image_digest' not in curr_evaluation_result:
+        # TODO hack! Include image digest and status, needed for the downstream notifications handler
+        if curr_evaluation_result:
             curr_evaluation_result['image_digest'] = imageDigest
+            if curr_final_action in ['GO', 'WARN']:
+                curr_evaluation_result['status'] = 'pass'
+            else:
+                curr_evaluation_result['status'] = 'fail'
         
         # set up the newest evaluation
         evalId = hashlib.md5(':'.join([policyId, userId, imageDigest, fulltag, str(curr_final_action)]).encode('utf8')).hexdigest()
@@ -1291,9 +1295,12 @@ def perform_policy_evaluation(userId, imageDigest, dbsession, evaltag=None, poli
                 try:
                     last_evaluation_result = obj_store.get_document(userId, 'policy_evaluations', last_evaluation_record['evalId'])
                     last_final_action = last_evaluation_result['final_action'].upper()
-                    # TODO hack! Include image digest, needed for the downstream notifications handler
-                    if 'image_digest' not in last_evaluation_result:
-                        last_evaluation_result['image_digest'] = imageDigest
+                    # TODO hack! Include image digest and status, needed for the downstream notifications handler
+                    last_evaluation_result['image_digest'] = imageDigest
+                    if last_final_action in ['GO', 'WARN']:
+                        last_evaluation_result['status'] = 'pass'
+                    else:
+                        last_evaluation_result['status'] = 'fail'
                 except:
                     logger.warn("no last eval record - skipping")
 
