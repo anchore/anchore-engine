d961a3ce4a653b8545477a3615a5deffc2910e3f
Dan Luhring <dan.luhring@anchore.com>
2020-08-02 21:03:25 -0400

    Leverage pytest and parameterization
    Signed-off-by: Dan Luhring <dan.luhring@anchore.com>

--- tests/unit/anchore_engine/auth/test_common.py
+++ tests/unit/anchore_engine/auth/test_common.py
@@ -8,123 +8,81 @@ _test_password = 'potts'
 _test_registry_meta = {
     'authorizationToken': '{}:{}'.format(_test_username, _test_password)
 }
+_record_ecr = {
+    'registry_type': 'awsecr',
+    'registry_meta': json.dumps(_test_registry_meta),
+}
+_record_not_ecr = {
+    'registry_type': 'other-registry',
+    'registry_user': _test_username,
+    'registry_pass': _test_password,
+}
+_record_ecr_inactive = {
+    'registry': 'docker.io',
+    'record_state_key': 'inactive',
+    'registry_type': 'awsecr',
+    'registry_meta': json.dumps(_test_registry_meta),
+    'registry_verify': True,
+}
+_record_ecr_unavailable = {
+    'registry': 'docker.io',
+    'record_state_key': 'inactive',
+    'record_state_val': time.time(), # note: technically this could yield nondeterministic results
+    'registry_type': 'awsecr',
+    'registry_meta': json.dumps(_test_registry_meta),
+    'registry_verify': True,
+}
 
-def test_get_docker_registry_userpw():
-    record_ecr = {
-        'registry_type': 'awsecr',
-        'registry_meta': json.dumps(_test_registry_meta),
-    }
+@pytest.mark.parametrize("registry_record", [
+    _record_ecr,
+    _record_not_ecr
+])
+def test_get_docker_registry_userpw(registry_record):
+    result = get_docker_registry_userpw(registry_record)
+    assert result == (_test_username, _test_password)
+
+def test_get_docker_registry_userpw_bad_json():
     record_ecr_bad_json = {
         'registry_type': 'awsecr',
         'registry_meta': 'this-is-not-valid-json!}',
     }
-    record_not_ecr = {
-        'registry_type': 'other-registry',
-        'registry_user': _test_username,
-        'registry_pass': _test_password,
-    }
 
-    test_cases = [
-        {
-            'record': record_ecr,
-            'expected': (_test_username, _test_password),
-        },
-        {
-            'record': record_ecr_bad_json,
-            'should_raise_exception': True,
-        },
-        {
-            'record': record_not_ecr,
-            'expected': (_test_username, _test_password),
-        },
-    ]
+    with pytest.raises(Exception):
+        get_docker_registry_userpw(record_ecr_bad_json)
 
-    for case in test_cases:
-        try:
-            result = get_docker_registry_userpw(case['record'])
-            assert(result == case['expected'])
-        except Exception:
-            assert(case['should_raise_exception'])
+@pytest.mark.parametrize("registry,repository,registry_creds,expected", [
+    ("docker.io", "library/node", None, (None, None, None)),
+    ("docker.io", "library/node", [_record_ecr_inactive], (_test_username, _test_password, True)),
+])
+def test_get_creds_by_registry(registry, repository, registry_creds, expected):
+    result = get_creds_by_registry(registry, repository, registry_creds)
+    assert result == expected
 
+def test_get_creds_by_registry_unavailable():
+    with pytest.raises(Exception):
+        get_creds_by_registry('docker.io', 'library/node', [_record_ecr_unavailable])
 
-def test_get_creds_by_registry():
-    registry_verify = True
+@pytest.mark.parametrize("registry_record_str,registry,repository", [
+    ('docker.io/library/centos', 'docker.io', 'library/centos'),
+    ('docker.io', 'docker.io', 'centos'),
+    ('docker.io', 'docker.io', 'myuser/myrepo')
+])
+def test_registry_record_matches_exact(registry_record_str, registry, repository):
+    assert registry_record_matches(registry_record_str, registry, repository)
 
-    creds = [
-        {
-            'registry': 'docker.io',
-            'record_state_key': 'inactive',
-            'registry_type': 'awsecr',
-            'registry_meta': json.dumps(_test_registry_meta),
-            'registry_verify': registry_verify,
-        }
-    ]
+@pytest.mark.parametrize("registry_record_str,registry,repository", [
+    ('docker.io/library/*', 'docker.io', 'library/centos'),
+    ('docker.io/*', 'docker.io', 'library/centos'),
+    ('gcr.io/myproject/*', 'gcr.io', 'myproject/myuser/myrepo')
+])
+def test_registry_record_matches_wildcard(registry_record_str, registry, repository):
+    assert registry_record_matches(registry_record_str, registry, repository)
 
-    creds_for_unavailable_registry = [
-        {
-            'registry': 'docker.io',
-            'record_state_key': 'inactive',
-            'record_state_val': time.time(), # note: technically this could yield nondeterministic results
-            'registry_type': 'awsecr',
-            'registry_meta': json.dumps(_test_registry_meta),
-            'registry_verify': registry_verify,
-        }
-    ]
-
-    test_cases = [
-        {
-            'registry': 'docker.io',
-            'repository': 'library/node',
-            'registry_creds': None,
-            'expected': (None, None, None)
-        },
-        {
-            'registry': 'docker.io',
-            'repository': 'library/node',
-            'registry_creds': creds,
-            'expected': (_test_username, _test_password, registry_verify)
-        },
-        {
-            'registry': 'docker.io',
-            'repository': 'library/node',
-            'registry_creds': creds_for_unavailable_registry,
-            'should_raise_exception': True,
-        }
-    ]
-
-    for case in test_cases:
-        try:
-            result = get_creds_by_registry(case['registry'], case['repository'], case['registry_creds'])
-            assert(result == case['expected'])
-        except Exception:
-            assert(case['should_raise_exception'])
-
-
-def test_registry_record_matches():
-    exact_matches = [
-        ('docker.io/library/centos', 'docker.io', 'library/centos'),
-        ('docker.io', 'docker.io', 'centos'),
-        ('docker.io', 'docker.io', 'myuser/myrepo')
-    ]
-
-    wildcard_matches = [
-        ('docker.io/library/*', 'docker.io', 'library/centos'),
-        ('docker.io/*', 'docker.io', 'library/centos'),
-        ('gcr.io/myproject/*', 'gcr.io', 'myproject/myuser/myrepo')
-    ]
-
-    non_match = [
-        ('docker.io', 'gcr.io', 'myproject/myuser'),
-        ('docker.io/*', 'gcr.io', 'myproject/myuser'),
-        ('docker.io/library/*', 'docker.io', 'myuser/myrepo'),
-        ('docker.io/myuser/myrepo', 'docker.io', 'myuser/myrepo2')
-    ]
-
-    for test in exact_matches:
-        assert(registry_record_matches(test[0], test[1], test[2]))
-
-    for test in wildcard_matches:
-        assert(registry_record_matches(test[0], test[1], test[2]))
-
-    for test in non_match:
-        assert(not registry_record_matches(test[0], test[1], test[2]))
+@pytest.mark.parametrize("registry_record_str,registry,repository", [
+    ('docker.io', 'gcr.io', 'myproject/myuser'),
+    ('docker.io/*', 'gcr.io', 'myproject/myuser'),
+    ('docker.io/library/*', 'docker.io', 'myuser/myrepo'),
+    ('docker.io/myuser/myrepo', 'docker.io', 'myuser/myrepo2')
+])
+def test_registry_record_matches_non(registry_record_str, registry, repository):
+    assert not registry_record_matches(registry_record_str, registry, repository)
