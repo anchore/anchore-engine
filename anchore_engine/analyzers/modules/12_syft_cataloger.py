02ae68306a61143a6f192694c678b21d45721050
Alex Goodman <alex.goodman@anchore.com>
2020-10-01 09:37:21 -0400

    helpful debugging

---
+++ anchore_engine/analyzers/modules/12_syft_cataloger.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+
+import sys
+import os
+import re
+import json
+import tarfile
+
+import anchore_engine.analyzers.utils
+
+
+def main(config):
+    imgname = config['imgid']
+    print(f"We have an image! f{imgname}")
+
+if __name__ == "__main__":
+
+    try:
+        config = anchore_engine.analyzers.utils.init_analyzer_cmdline(sys.argv, "syft_cataloger")
+    except Exception as err:
+        # TODO: improve me
+        print(str(err))
+        sys.exit(1)
+
+    main(config)
