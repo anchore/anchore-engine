5c877bbe5500a84f12959f9deff1a10c497876e7
Alfredo Deza <adeza@anchore.com>
2020-10-26 10:43:54 -0400

    analyzers: replace npm with syft
    Signed-off-by: Alfredo Deza <adeza@anchore.com>
    Signed-off-by: Alex Goodman <alex.goodman@anchore.com>

--- anchore_engine/analyzers/syft.py
+++ anchore_engine/analyzers/syft.py
@@ -1,8 +1,6 @@
 #!/usr/bin/env python3
 
 import os
-import sys
-import json
 import collections
 
 import anchore_engine.analyzers.utils
@@ -17,7 +15,7 @@ def handle_python(findings, artifact):
     site_pkg_root = artifact['metadata']['sitePackagesRootPath']
 
     # anchore engine always uses the name, however, the name may not be a top-level package
-    # instead default to the first top-level package unless the name is listed among the 
+    # instead default to the first top-level package unless the name is listed among the
     # top level packages explicitly defined in the metadata
     pkg_key_name = artifact['metadata']['topLevelPackages'][0]
     if artifact['name'] in artifact['metadata']['topLevelPackages']:
@@ -28,7 +26,7 @@ def handle_python(findings, artifact):
     email = artifact['metadata'].get('authorEmail', None)
     if email:
         origin += " <%s>" % email
-    
+
     files = []
     for file in artifact['metadata'].get('files', []):
         files.append(os.path.join(site_pkg_root, file['path']))
@@ -48,6 +46,7 @@ def handle_python(findings, artifact):
     # inject the artifact document into the "raw" analyzer document
     findings['package_list']['pkgs.python']['base'][pkg_key] = pkg_value
 
+
 def handle_gem(findings, artifact):
     pkg_key = artifact['locations'][0]['path']
 
@@ -65,9 +64,32 @@ def handle_gem(findings, artifact):
     # inject the artifact document into the "raw" analyzer document
     findings['package_list']['pkgs.gems']['base'][pkg_key] = pkg_value
 
+
+def handle_npm(findings, artifact):
+    pkg_key = artifact['locations'][0]['path']
+    homepage = artifact['metadata'].get('homepage', '')
+    author = artifact['metadata'].get('author')
+    authors = artifact['metadata'].get('authors', [])
+    origins = [] if not author else [author]
+    origins.extend(authors)
+
+    pkg_value = {
+            'name': artifact['name'],
+            'versions': [artifact['version']],
+            'latest': artifact['version'],
+            'sourcepkg': artifact['metadata'].get('url', homepage),
+            'origins': origins,
+            'lics': artifact['metadata'].get('licenses', []),
+        }
+
+    # inject the artifact document into the "raw" analyzer document
+    findings['package_list']['pkgs.npms']['base'][pkg_key] = pkg_value
+
+
 def filter_artifacts(artifact):
     # only allow artifacts which have handlers implemented, ignore the rest
     return artifact['type'] in artifact_handler_dispatch
+
 
 def catalog_image(image):
     all_results = run_syft(image)
@@ -76,7 +98,9 @@ def catalog_image(image):
     nested_dict = lambda: collections.defaultdict(nested_dict)
     findings = nested_dict()
 
-    # take a sub-set of the syft findings and invoke the handler function to craft the artifact document and inject into the "raw" analyzer json document
+    # take a sub-set of the syft findings and invoke the handler function to
+    # craft the artifact document and inject into the "raw" analyzer json
+    # document
     for artifact in filter(filter_artifacts, all_results['artifacts']):
         artifact_handler_dispatch[artifact['type']](findings, artifact)
 
@@ -85,6 +109,7 @@ def catalog_image(image):
 
 artifact_handler_dispatch = {
     'gem': handle_gem,
-    'python': handle_python
+    'python': handle_python,
+    'npm': handle_npm,
     # TODO: add handlers as we go...
 }
