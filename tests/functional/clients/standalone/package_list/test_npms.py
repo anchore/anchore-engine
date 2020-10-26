c81618954ca79322ad9e92cf6d10691b2f37fcaa
Alfredo Deza <adeza@anchore.com>
2020-10-26 10:29:14 -0400

    tests: update npm fixtures to use latest
    Signed-off-by: Alfredo Deza <adeza@anchore.com>
    Signed-off-by: Alex Goodman <alex.goodman@anchore.com>

--- tests/functional/clients/standalone/package_list/test_npms.py
+++ tests/functional/clients/standalone/package_list/test_npms.py
@@ -19,40 +19,40 @@ class TestJSMetadata:
     def test_has_name(self, analyzed_data, path, metadata):
         result = analyzed_data('npm')
         pkgs = result['image']['imagedata']['analysis_report']['package_list']['pkgs.npms']['base']
-        loaded = pkgs.get(path, '{}')
+        loaded = pkgs.get(path, {})
         assert loaded['name'] == metadata['name']
 
     @pytest.mark.parametrize('path,metadata', metadata_params(npms.pkgs))
     def test_has_lics(self, analyzed_data, path, metadata):
         result = analyzed_data('npm')
         pkgs = result['image']['imagedata']['analysis_report']['package_list']['pkgs.npms']['base']
-        loaded = pkgs.get(path, '{}')
+        loaded = pkgs.get(path, {})
         assert loaded['lics'] == metadata['lics']
 
     @pytest.mark.parametrize('path,metadata', metadata_params(npms.pkgs))
     def test_has_versions(self, analyzed_data, path, metadata):
         result = analyzed_data('npm')
         pkgs = result['image']['imagedata']['analysis_report']['package_list']['pkgs.npms']['base']
-        loaded = pkgs.get(path, '{}')
+        loaded = pkgs.get(path, {})
         assert loaded['versions'] == metadata['versions']
 
     @pytest.mark.parametrize('path,metadata', metadata_params(npms.pkgs))
     def test_has_latest(self, analyzed_data, path, metadata):
         result = analyzed_data('npm')
         pkgs = result['image']['imagedata']['analysis_report']['package_list']['pkgs.npms']['base']
-        loaded = pkgs.get(path, '{}')
+        loaded = pkgs.get(path, {})
         assert loaded['latest'] == metadata['latest']
 
     @pytest.mark.parametrize('path,metadata', metadata_params(npms.pkgs))
     def test_has_origins(self, analyzed_data, path, metadata):
         result = analyzed_data('npm')
         pkgs = result['image']['imagedata']['analysis_report']['package_list']['pkgs.npms']['base']
-        loaded = pkgs.get(path, '{}')
+        loaded = pkgs.get(path, {})
         assert loaded['origins'] == metadata['origins']
 
     @pytest.mark.parametrize('path,metadata', metadata_params(npms.pkgs))
     def test_has_sourcepkg(self, analyzed_data, path, metadata):
         result = analyzed_data('npm')
         pkgs = result['image']['imagedata']['analysis_report']['package_list']['pkgs.npms']['base']
-        loaded = pkgs.get(path, '{}')
+        loaded = pkgs.get(path, {})
         assert loaded['sourcepkg'] == metadata['sourcepkg']
