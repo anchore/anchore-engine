3911cce42e704614213258f2e7e1b6a89da2a4a7
Alex Goodman <alex.goodman@anchore.com>
2020-11-02 16:59:02 -0500

    reduce alpine test cases
    Signed-off-by: Alex Goodman <alex.goodman@anchore.com>

--- tests/functional/clients/standalone/package_list/test_alpine.py
+++ tests/functional/clients/standalone/package_list/test_alpine.py
@@ -23,10 +23,23 @@ class TestAlpinePaths:
     def test_pkgs_allinfo(self, analyzed_data, pkg, metadata):
         result = analyzed_data("py38")
         pkgs = result['image']['imagedata']['analysis_report']['package_list']['pkgs.allinfo']['base']
-        loaded = json.loads(pkgs.get(pkg, '{}'))
+        loaded = dict(json.loads(pkgs.get(pkg, '{}')))
+        
+        # a separate test exists just for the files attribute, leave this out of the assertion
+        loaded.pop('files')
+        metadata = dict(metadata)
+        metadata.pop('files')
+
         assert loaded == metadata
 
-    @pytest.mark.parametrize('pkg,version', [pytest.param(pkg, version, id=pkg) for pkg, version in alpine.pkgs_all.items()])
+    @pytest.mark.parametrize('pkg,metadata', alpine.pkgs_allinfo.items())
+    def test_pkgs_allinfo_files(self, analyzed_data, pkg, metadata):
+        result = analyzed_data("py38")
+        pkgs = result['image']['imagedata']['analysis_report']['package_list']['pkgs.allinfo']['base']
+        loaded = json.loads(pkgs.get(pkg, '{}'))
+        assert sorted(loaded['files']) == sorted(metadata['files'])
+
+    @pytest.mark.parametrize('pkg,version', [pytest.param(pkg, version, id=pkg) for pkg, version in alpine.pkgs_plus_source_all.items()])
     def test_pkgs_plus_source_all(self, analyzed_data, pkg, version):
         result = analyzed_data("py38")
         pkgs = result['image']['imagedata']['analysis_report']['package_list']['pkgs_plus_source.all']['base']
