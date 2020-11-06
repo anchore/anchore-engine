bbcbda2f1338febd0de5aa037e5ecffa2286e56f
Alex Goodman <alex.goodman@anchore.com>
2020-11-06 13:30:57 -0500

    replace dpkg analyzer module with syft call
    Signed-off-by: Alex Goodman <alex.goodman@anchore.com>

--- tests/functional/clients/standalone/package_list/test_debian.py
+++ tests/functional/clients/standalone/package_list/test_debian.py
@@ -7,29 +7,41 @@ import pytest
 
 class TestDebianPaths:
 
-    @pytest.mark.skipif(sys.platform == "darwin", reason="no dpkg on OSX")
     @pytest.mark.parametrize('path', path_params(debian.pkgfiles_all))
     def test_pkgfiles_all(self, analyzed_data, path):
         result = analyzed_data("stretch-slim")
         pkgs = result['image']['imagedata']['analysis_report']['package_list']['pkgfiles.all']['base']
         assert pkgs.get(path) == 'DPKGFILE'
 
-    @pytest.mark.skipif(sys.platform == "darwin", reason="no dpkg on OSX")
     @pytest.mark.parametrize('pkg,version', [pytest.param(pkg, version, id=pkg) for pkg, version in debian.pkgs_all.items()])
     def test_pkgs_all(self, analyzed_data, pkg, version):
         result = analyzed_data("stretch-slim")
         pkgs = result['image']['imagedata']['analysis_report']['package_list']['pkgs.all']['base']
         assert pkgs.get(pkg) == version
 
-    @pytest.mark.skipif(sys.platform == "darwin", reason="no dpkg on OSX")
     @pytest.mark.parametrize('pkg,metadata', debian.pkgs_allinfo.items())
     def test_pkgs_allinfo(self, analyzed_data, pkg, metadata):
         result = analyzed_data("stretch-slim")
         pkgs = result['image']['imagedata']['analysis_report']['package_list']['pkgs.allinfo']['base']
-        loaded = json.loads(pkgs.get(pkg, '{}'))
+        loaded = pkgs.get(pkg, {})
+
+        # a separate test exists just for the licenses attribute, leave this out of the assertion
+        loaded = dict(loaded)
+        loaded.pop('license')
+        metadata = dict(metadata)
+        metadata.pop('license')
+
         assert loaded == metadata
 
-    @pytest.mark.skipif(sys.platform == "darwin", reason="no dpkg on OSX")
+    @pytest.mark.parametrize('pkg,metadata', debian.pkgs_allinfo.items())
+    def test_pkgs_allinfo_license(self, analyzed_data, pkg, metadata):
+        result = analyzed_data("stretch-slim")
+        pkgs = result['image']['imagedata']['analysis_report']['package_list']['pkgs.allinfo']['base']
+        loaded = pkgs.get(pkg, {})
+        actual = sorted(loaded['license'])
+        expected = sorted(metadata['license'])
+        assert actual == expected
+
     @pytest.mark.parametrize('pkg,version', [pytest.param(pkg, version, id=pkg) for pkg, version in debian.pkgs_all.items()])
     def test_pkgs_plus_source_all(self, analyzed_data, pkg, version):
         result = analyzed_data("stretch-slim")
