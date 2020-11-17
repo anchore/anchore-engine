b732bdb449a1d76a7023480b2cf5de93020ffc00
Zane Burstein <zane.burstein@anchore.com>
2020-11-17 09:50:09 -0800

    Parameterized all tests to a single test function
    Signed-off-by: Zane Burstein <zane.burstein@anchore.com>

--- tests/unit/anchore_engine/services/policy_engine/policy/gates/test_dockerfile.py
+++ tests/unit/anchore_engine/services/policy_engine/policy/gates/test_dockerfile.py
@@ -5,7 +5,6 @@ from anchore_engine.db.entities.policy_engine import Image
 
 image_id = "1"
 user = "admin"
-absent_modes = ["guessed", None]
 dockerfile_contents = """FROM ubuntu:18.04
 USER admin
 COPY . /app
@@ -16,14 +15,16 @@ EXPOSE 3000
 
 
 @pytest.fixture()
-def image():
-    img = Image()
-    img.id = image_id
-    img.user_id = user
-    img.dockerfile_contents = dockerfile_contents
-    img.dockerfile_mode = "actual"
+def create_image():
+    def _create_image(dockerfile_mode="actual"):
+        img = Image()
+        img.id = image_id
+        img.user_id = user
+        img.dockerfile_contents = dockerfile_contents
+        img.dockerfile_mode = dockerfile_mode
+        return img
 
-    return img
+    return _create_image
 
 
 @pytest.fixture()
@@ -32,13 +33,13 @@ def dockerfile_gate():
 
 
 @pytest.fixture()
-def no_dockerfile_trigger(dockerfile_gate):
-    return dockerfile.NoDockerfile(parent_gate_cls=dockerfile_gate.__class__)
+def exec_context():
+    return ExecutionContext(db_session=None, configuration={})
 
 
 @pytest.fixture()
-def exec_context():
-    return ExecutionContext(db_session=None, configuration={})
+def no_dockerfile_trigger(dockerfile_gate):
+    return dockerfile.NoDockerfile(parent_gate_cls=dockerfile_gate.__class__)
 
 
 @pytest.fixture()
@@ -72,154 +73,131 @@ def make_instruction_check_trigger(dockerfile_gate):
 
 
 @pytest.fixture()
-def make_actual_dockerfile_only_trigger(
-    dockerfile_gate, make_instruction_check_trigger, make_exposed_ports_trigger
+def make_trigger(
+    dockerfile_gate,
+    make_effective_user_trigger,
+    make_exposed_ports_trigger,
+    make_instruction_check_trigger,
+    no_dockerfile_trigger,
 ):
     def _make_trigger(type, trigger_params):
-        if type == "instruction_check":
-            return make_instruction_check_trigger(
-                {"actual_dockerfile_only": True, **trigger_params}
-            )
+        if type == "no_dockerfile":
+            return no_dockerfile_trigger
+        elif type == "effective_user":
+            return make_effective_user_trigger({**trigger_params})
+        elif type == "instruction_check":
+            return make_instruction_check_trigger({**trigger_params})
         elif type == "exposed_port":
-            return make_exposed_ports_trigger(
-                {"actual_dockerfile_only": True, **trigger_params}
-            )
+            return make_exposed_ports_trigger({**trigger_params})
 
     return _make_trigger
 
 
-def test_dockerfile_present(
-    dockerfile_gate, no_dockerfile_trigger, exec_context, image
-):
-    dockerfile_gate.prepare_context(image, exec_context)
-    assert no_dockerfile_trigger.execute(image, exec_context)
-    assert no_dockerfile_trigger.did_fire is False
-
-
-@pytest.mark.parametrize("mode", absent_modes)
-def test_dockerfile_absent(
-    dockerfile_gate, no_dockerfile_trigger, exec_context, image, mode
-):
-    image.dockerfile_mode = mode
-
-    dockerfile_gate.prepare_context(image, exec_context)
-    assert no_dockerfile_trigger.execute(image, exec_context)
-    assert no_dockerfile_trigger.did_fire
-    assert no_dockerfile_trigger.fired[0].msg == dockerfile.NoDockerfile.__msg__
-
-
-# Effective user tests parameterized via dict objects
-# Tests in order: user in blacklist, user not in blacklist, user in whitelist, user not in whitelist
-effective_user_test_contexts = [
+no_dockerfile_trigger_tests = [
+    {"trigger_type": "no_dockerfile", "trigger_params": {}, "expected_fire": False},
     {
+        "trigger_type": "no_dockerfile",
+        "trigger_params": {},
+        "expected_fire": True,
+        "dockerfile_mode": "guessed",
+        "expected_msg": dockerfile.NoDockerfile.__msg__,
+    },
+    {
+        "trigger_type": "no_dockerfile",
+        "trigger_params": {},
+        "expected_fire": True,
+        "dockerfile_mode": None,
+        "expected_msg": dockerfile.NoDockerfile.__msg__,
+    },
+]
+
+effective_user_trigger_tests = [
+    {
+        "trigger_type": "effective_user",
         "trigger_params": {"users": "testUser,admin", "type": "blacklist"},
         "expected_fire": True,
         "expected_msg": "User admin found as effective user, which is explicity not allowed",
     },
     {
+        "trigger_type": "effective_user",
         "trigger_params": {"users": "testUser", "type": "blacklist"},
         "expected_fire": False,
     },
     {
+        "trigger_type": "effective_user",
         "trigger_params": {"users": "admin", "type": "whitelist"},
         "expected_fire": False,
     },
     {
+        "trigger_type": "effective_user",
         "trigger_params": {"users": "admin", "type": "blacklist"},
         "expected_fire": True,
         "expected_msg": "User admin found as effective user, which is explicity not allowed",
     },
 ]
 
-
-@pytest.mark.parametrize("test_context", effective_user_test_contexts)
-def test_effective_user_trigger(
-    dockerfile_gate, make_effective_user_trigger, exec_context, image, test_context
-):
-    effective_user_trigger = make_effective_user_trigger(test_context["trigger_params"])
-
-    dockerfile_gate.prepare_context(image, exec_context)
-    assert effective_user_trigger.execute(image, exec_context)
-
-    if test_context["expected_fire"]:
-        assert effective_user_trigger.did_fire
-        assert len(effective_user_trigger.fired) == 1
-        assert effective_user_trigger.fired[0].msg == test_context["expected_msg"]
-    else:
-        assert effective_user_trigger.did_fire is False
-
-
-# exposed ports parameterized via dicts
-# Tests in order: port in blacklist, port no in blacklist, port in whitelist, port not in whitelist
-exposed_ports_test_contexts = [
+exposed_port_trigger_tests = [
     {
+        "trigger_type": "exposed_port",
         "trigger_params": {"ports": "2800,3000", "type": "blacklist"},
         "expected_fire": True,
         "expected_msg": "Dockerfile exposes port (3000) which is in policy file DENIEDPORTS list",
     },
     {
+        "trigger_type": "exposed_port",
         "trigger_params": {"ports": "2800", "type": "blacklist"},
         "expected_fire": False,
     },
     {
+        "trigger_type": "exposed_port",
         "trigger_params": {"ports": "3000", "type": "whitelist"},
         "expected_fire": False,
     },
     {
+        "trigger_type": "exposed_port",
         "trigger_params": {"ports": "2800,3001", "type": "whitelist"},
         "expected_fire": True,
         "expected_msg": "Dockerfile exposes port (3000) which is not in policy file ALLOWEDPORTS list",
     },
 ]
 
-
-@pytest.mark.parametrize("test_context", exposed_ports_test_contexts)
-def test_exposed_ports_trigger(
-    dockerfile_gate, make_exposed_ports_trigger, exec_context, image, test_context
-):
-    exposed_ports_trigger = make_exposed_ports_trigger(test_context["trigger_params"])
-
-    dockerfile_gate.prepare_context(image, exec_context)
-    assert exposed_ports_trigger.execute(image, exec_context)
-
-    if test_context["expected_fire"]:
-        assert exposed_ports_trigger.did_fire
-        assert len(exposed_ports_trigger.fired) == 1
-        assert exposed_ports_trigger.fired[0].msg == test_context["expected_msg"]
-    else:
-        assert exposed_ports_trigger.did_fire is False
-
-
-instruction_check_test_contexts = [
+instruction_check_trigger_tests = [
     {
+        "trigger_type": "instruction_check",
         "trigger_params": {"instruction": "COPY", "check": "=", "value": ". /app"},
         "expected_fire": True,
         "expected_msg": "Dockerfile directive 'COPY' check '=' matched against '. /app' for line '. /app'",
     },
     {
+        "trigger_type": "instruction_check",
         "trigger_params": {"instruction": "COPY", "check": "!=", "value": ". /app"},
         "expected_fire": False,
     },
     {
+        "trigger_type": "instruction_check",
         "trigger_params": {"instruction": "COPY", "check": "exists"},
         "expected_fire": True,
         "expected_msg": "Dockerfile directive 'COPY' check 'exists' matched against '' for line '. /app'",
     },
     {
+        "trigger_type": "instruction_check",
         "trigger_params": {"instruction": "VOLUME", "check": "not_exists"},
         "expected_fire": True,
         "expected_msg": "Dockerfile directive 'VOLUME' not found, matching condition 'not_exists' check",
     },
     {
+        "trigger_type": "instruction_check",
         "trigger_params": {"instruction": "COPY", "check": "not_exists"},
         "expected_fire": False,
     },
     {
+        "trigger_type": "instruction_check",
         "trigger_params": {"instruction": "COPY", "check": "like", "value": ". /"},
         "expected_fire": True,
         "expected_msg": "Dockerfile directive 'COPY' check 'like' matched against '. /' for line '. /app'",
     },
     {
+        "trigger_type": "instruction_check",
         "trigger_params": {
             "instruction": "COPY",
             "check": "like",
@@ -228,6 +206,7 @@ instruction_check_test_contexts = [
         "expected_fire": False,
     },
     {
+        "trigger_type": "instruction_check",
         "trigger_params": {
             "instruction": "COPY",
             "check": "not_like",
@@ -237,6 +216,7 @@ instruction_check_test_contexts = [
         "expected_msg": "Dockerfile directive 'COPY' check 'not_like' matched against '. /usr' for line '. /app'",
     },
     {
+        "trigger_type": "instruction_check",
         "trigger_params": {
             "instruction": "COPY",
             "check": "not_like",
@@ -246,52 +226,53 @@ instruction_check_test_contexts = [
     },
 ]
 
-
-@pytest.mark.parametrize("test_context", instruction_check_test_contexts)
-def test_instruction_check_trigger(
-    dockerfile_gate, make_instruction_check_trigger, exec_context, image, test_context
-):
-    instruction_check_trigger = make_instruction_check_trigger(
-        test_context["trigger_params"]
-    )
-
-    dockerfile_gate.prepare_context(image, exec_context)
-    assert instruction_check_trigger.execute(image, exec_context)
-
-    if test_context["expected_fire"]:
-        assert instruction_check_trigger.did_fire
-        assert len(instruction_check_trigger.fired) == 1
-        assert instruction_check_trigger.fired[0].msg == test_context["expected_msg"]
-    else:
-        assert instruction_check_trigger.did_fire is False
-
-
-actual_dockerfile_only_test_contexts = [
+actual_dockerfile_only_tests = [
     {
         "trigger_type": "instruction_check",
-        "trigger_params": {"instruction": "COPY", "check": "exists"},
+        "trigger_params": {
+            "actual_dockerfile_only": True,
+            "instruction": "COPY",
+            "check": "exists",
+        },
+        "expected_fire": False,
+        "dockerfile_mode": None,
     },
     {
         "trigger_type": "exposed_port",
-        "trigger_params": {"ports": "3000", "type": "blacklist"},
+        "trigger_params": {
+            "actual_dockerfile_only": True,
+            "ports": "3000",
+            "type": "blacklist",
+        },
+        "expected_fire": False,
+        "dockerfile_mode": None,
     },
 ]
 
+test_contexts = [
+    *no_dockerfile_trigger_tests,
+    *effective_user_trigger_tests,
+    *exposed_port_trigger_tests,
+    *instruction_check_trigger_tests,
+    *actual_dockerfile_only_tests,
+]
 
-@pytest.mark.parametrize("test_context", actual_dockerfile_only_test_contexts)
-def test_actual_dockerfile_only(
-    dockerfile_gate,
-    exec_context,
-    image,
-    test_context,
-    make_actual_dockerfile_only_trigger,
+
+@pytest.mark.parametrize("test_context", test_contexts)
+def test_variable_trigger_when_dockerfile_present(
+    dockerfile_gate, make_trigger, exec_context, create_image, test_context
 ):
-    image.dockerfile_mode = None
-
-    trigger = make_actual_dockerfile_only_trigger(
-        test_context["trigger_type"], test_context["trigger_params"]
+    image = create_image(test_context.get("dockerfile_mode", "actual"))
+    trigger = make_trigger(
+        test_context["trigger_type"], test_context.get("trigger_params", {})
     )
 
     dockerfile_gate.prepare_context(image, exec_context)
     assert trigger.execute(image, exec_context)
-    assert not trigger.did_fire
+
+    if test_context["expected_fire"]:
+        assert trigger.did_fire
+        assert len(trigger.fired) == 1
+        assert trigger.fired[0].msg == test_context["expected_msg"]
+    else:
+        assert trigger.did_fire is False
