7b1918872f6c22083bf39012298b06b300d31e08
Daniel Palmer <dan.palmer@anchore.com>
2021-04-19 16:49:46 -0400

    Change out locking solution to something off the shelf. Refactor to expect grype db as a path to tarball. Add logging and some exception catching. Add multiple tests and test files
    Signed-off-by: Daniel Palmer <dan.palmer@anchore.com>

--- anchore_engine/clients/grype_wrapper.py
+++ anchore_engine/clients/grype_wrapper.py
@@ -3,57 +3,103 @@ import os
 import json
 import shlex
 import sqlalchemy
-import uuid
+import tarfile
 
 from anchore_engine.db.entities.common import UtilMixin
 from anchore_engine.subsys import logger
-from anchore_engine.subsys.locking import ManyReadsOneWriteLock
-from anchore_engine.utils import run_check
+from anchore_engine.utils import run_check, CommandException
+from readerwriterlock import rwlock
 from sqlalchemy import Column, String, Integer
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
-from threading import Lock
 
 
 grype_db_file = None
 grype_db_session = None
-grype_db_lock = ManyReadsOneWriteLock()
-grype_db_write_lock = Lock()
+grype_db_lock = rwlock.RWLockWrite()
 
 Base = declarative_base()
+VULNERABILITY_FILE_NAME = "vulnerability.db"
 VULNERABILITY_TABLE_NAME = "vulnerability"
 VULNERABILITY_METADATA_TABLE_NAME = "vulnerability_metadata"
 
 
-def _get_default_cache_dir_from_config():
+# TODO Implement, once added to Grype
+def get_current_grype_db_checksum():
     """
-    Get the default grype db dir from config.
+    Return the checksum for the in-use version of grype db
+    """
+    grype_db_checksum = None
+    logger.info("Returning current grype_db checksum: {}".format(grype_db_checksum))
+    return grype_db_checksum
+
+
+def _get_default_grype_db_dir_from_config():
+    """
+    Get the default grype db dir from config, and create it if it does not exist.
     """
     localconfig = anchore_engine.configuration.localconfig.get_config()
-    if "grype_db_cache_dir" in localconfig:
-        full_path = os.path.join(localconfig["service_dir"], localconfig["grype_db_cache_dir"])
+    if "grype_db_dir" in localconfig:
+        grype_db_dir = os.path.join(localconfig["service_dir"], localconfig["grype_db_dir"])
     else:
-        full_path = os.path.join("/tmp/anchoretmp", "grype_db/")
+        grype_db_dir = os.path.join(localconfig["service_dir"], "grype_db/")
 
-    if not os.path.exists(full_path):
-        # TODO makedirs (plural) is probably wrong. Outside of unit tests service dir should already exist at this point.
-        os.makedirs(full_path)
+    if not os.path.exists(grype_db_dir):
+        os.mkdir(grype_db_dir)
 
-    return full_path
+    return grype_db_dir
 
 
-def _write_grype_db_to_file(lastest_grype_db):
+def _move_grype_db_archive(grype_db_archive_local_file_location: str, output_dir: str) -> str:
+    # Get the location to move the archive to
+    archive_file_name = os.path.basename(grype_db_archive_local_file_location)
+    grype_db_archive_copied_file_location = os.path.join(output_dir, archive_file_name)
+
+    # Move the archive file
+    logger.info("Moving the grype_db archive from {} to {}".format(grype_db_archive_local_file_location,
+                                                                   grype_db_archive_copied_file_location))
+    os.replace(grype_db_archive_local_file_location, grype_db_archive_copied_file_location)
+    return grype_db_archive_copied_file_location
+
+
+def _open_grype_db_archive(grype_db_archive_copied_file_location: str, output_dir: str) -> str:
+    logger.info("Unpacking the grype_db archive at {} into {}".format(grype_db_archive_copied_file_location, output_dir))
+
+    # Put the extracted files in the same dir as the archive
+    with tarfile.open(grype_db_archive_copied_file_location) as read_archive:
+        read_archive.extractall(output_dir)
+
+
+    # Return the full path to the vulnerability file
+    output_path = os.path.join(output_dir, VULNERABILITY_FILE_NAME)
+    logger.info("Returning the unpacked grype_db file at {}".format(output_path))
+    return output_path
+
+
+def _remove_grype_db_archive(grype_db_archive_local_file_location: str):
+    logger.info("Removing the now-unpacked grype_db archive at {}".format(grype_db_archive_local_file_location))
+    os.remove(grype_db_archive_local_file_location)
+
+
+def _move_and_open_grype_db_archive(grype_db_archive_local_file_location: str) -> str:
     """
-    Write the new grype db definition to file, and return the file
+    This function moves a tarball containing the latest grype db from a location on the local file system
+    into the configured grype db dir. It then extracts all files in the tarball and removes the then-unneeded
+    archive file.
     """
-    local_db_dir = _get_default_cache_dir_from_config()
-    # TODO If there is a unique and identifying file name we can get or derive upstream from the db metadata
-    # (ie something with the timestamp) that would be much preferable to a unique but meaningless uuid.
-    uuid_file_name = str(uuid.uuid4())
-    latest_grype_db_file = "{}{}".format(local_db_dir, uuid_file_name)
-    # Write the passed grype db to that file
-    with open(latest_grype_db_file, "w") as local_grype_db_output:
-        local_grype_db_output.write(lastest_grype_db)
+    # Get the location to copy the archive to
+    local_db_dir = _get_default_grype_db_dir_from_config()
+
+    # Copy the archive
+    grype_db_archive_copied_file_location = _move_grype_db_archive(grype_db_archive_local_file_location, local_db_dir)
+
+    # Unpack the archive
+    latest_grype_db_file = _open_grype_db_archive(grype_db_archive_copied_file_location, local_db_dir)
+
+    # Remove the unpacked archive
+    _remove_grype_db_archive(grype_db_archive_copied_file_location)
+
+    # Return the full path to the grype db file
     return latest_grype_db_file
 
 
@@ -61,6 +107,7 @@ def _init_grype_db_engine(latest_grype_db_file):
     """
     Create and return the sqlalchemy engine object
     """
+    logger.info("Creating new db engine based on the grype_db at {}".format(latest_grype_db_file))
     db_connect = "sqlite:///{}".format(latest_grype_db_file)
     latest_grype_db_engine = sqlalchemy.create_engine(db_connect, echo=True)
     return latest_grype_db_engine
@@ -70,17 +117,18 @@ def _init_grype_db_session(grype_db_engine):
     """
     Create and return the db session
     """
+    logger.info("Creating new db session from engine based on the grype_db at {}".format(grype_db_engine.url))
     SessionMaker = sessionmaker(bind=grype_db_engine)
     grype_db_session = SessionMaker()
     return grype_db_session
 
 
-def _init_grype_db(lastest_grype_db: str):
+def _init_grype_db(lastest_grype_db_archive: str):
     """
     Write the db string to file, create the engine, and create the session
     Return the file and session
     """
-    latest_grype_db_file = _write_grype_db_to_file(lastest_grype_db)
+    latest_grype_db_file = _move_and_open_grype_db_archive(lastest_grype_db_archive)
     latest_grype_db_engine = _init_grype_db_engine(latest_grype_db_file)
     latest_grype_db_session = _init_grype_db_session(latest_grype_db_engine)
 
@@ -90,63 +138,89 @@ def _init_grype_db(lastest_grype_db: str):
 
 def _remove_local_grype_db(grype_db_file):
     """
-    Remove the local grype db, either because we've replaced it, or it is new but not well-structured
+    Remove old the local grype db file
     """
     if os.path.exists(grype_db_file):
+        logger.info("Removing old grype_db at {}".format(grype_db_file))
         os.remove(grype_db_file)
     else:
         logger.error("Failed to remove grype db at {} as it cannot be found.".format(grype_db_file))
     return
 
 
-def update_grype_db(lastest_grype_db):
+def update_grype_db(grype_db_archive_local_file_location:str):
     """
     Update the installed grype db with the provided definition, and remove the old grype db file.
     This method does not validation of the db, and assumes it has passed any required validation upstream
     """
     global grype_db_file, grype_db_session
 
-    with grype_db_lock.write_lock():
-        # Store the db locally and
-        # Create the sqlalchemy engine for the new db
-        latest_grype_db_file, latest_grype_db_session = _init_grype_db(lastest_grype_db)
+    logger.info("Updating grype with a new grype_db archive from {}".format(grype_db_archive_local_file_location))
 
-        # Store the file and session variables globally
-        # For use during reads and to remove in the next update
-        old_grype_db_file = grype_db_file
-        grype_db_file = latest_grype_db_file
-        grype_db_session = latest_grype_db_session
+    write_lock = grype_db_lock.gen_wlock()
+    if write_lock.acquire(blocking=True, timeout=60):
+        try:
 
-        # Remove the old local db
-        _remove_local_grype_db(old_grype_db_file)
+            # Store the db locally and
+            # Create the sqlalchemy engine for the new db
+            latest_grype_db_file, latest_grype_db_session = _init_grype_db(grype_db_archive_local_file_location)
+
+            # Store the file and session variables globally
+            # For use during reads and to remove in the next update
+            old_grype_db_file = grype_db_file
+            grype_db_file = latest_grype_db_file
+            grype_db_session = latest_grype_db_session
+
+            # Remove the old local db
+            _remove_local_grype_db(old_grype_db_file)
+        finally:
+            write_lock.release()
 
 
-def get_vulnerabilities(grype_sbom: str):
+
+def get_vulnerabilities(grype_sbom: str) -> json:
     """
     Use grype to scan the provided sbom for vulnerabilites.
     """
     global grype_db_file
 
     # Get the read lock
-    with grype_db_lock.read_access():
-        # Set grype env variables, including the grype db location
-        grype_env = {
-            "GRYPE_CHECK_FOR_APP_UPDATE": "0",
-            "GRYPE_LOG_STRUCTURED": "1",
-            "GRYPE_DB_CACHE_DIR": "{}".format(grype_db_file),
-        }
+    read_lock = grype_db_lock.gen_rlock()
+    if read_lock.acquire(blocking=False, timeout=60):
+        try:
+            # Set grype env variables, including the grype db location
+            grype_env = {
+                "GRYPE_CHECK_FOR_APP_UPDATE": "0",
+                "GRYPE_LOG_STRUCTURED": "1",
+                "GRYPE_DB_AUTO_UPDATE": "0",
+                "GRYPE_DB_CACHE_DIR": "{}".format(grype_db_file),
+            }
 
-        proc_env = os.environ.copy()
-        proc_env.update(grype_env)
+            proc_env = os.environ.copy()
+            proc_env.update(grype_env)
 
-        # Format and run the command
-        cmd = "grype -vv -o json sbom:{sbom}".format(
-            sbom=grype_sbom,
-        )
-        stdout, _ = run_check(shlex.split(cmd), env=proc_env)
+            # Format and run the command
+            cmd = "grype -vv -o json sbom:{sbom}".format(
+                sbom=grype_sbom,
+            )
+
+            logger.debug("Running grype with command: {}".format(cmd))
+
+            stdout = None
+            err = None
+            try:
+                stdout, _ = run_check(shlex.split(cmd), env=proc_env)
+            except CommandException as exc:
+                logger.error("Exception running command: {}, stderr: {}".format(cmd, exc.stderr))
+                raise exc
+        finally:
+            read_lock.release()
+
 
         # Return the output as json
         return json.loads(stdout)
+
+
 
 
 # Table definitions.
@@ -191,33 +265,40 @@ def query_vulnerabilities(
     global grype_db_session
 
     # Get and release read locks
-    with grype_db_lock.read_access():
-        if type(vuln_id) == str:
-            vuln_id = [vuln_id]
+    read_lock = grype_db_lock.gen_rlock()
+    if read_lock.acquire(blocking=False, timeout=60):
+        try:
+            if type(vuln_id) == str:
+                vuln_id = [vuln_id]
 
-        if type(namespace) == str:
-            namespace = [namespace]
+            if type(namespace) == str:
+                namespace = [namespace]
 
-        query = grype_db_session.query(
-            GrypeVulnerability, GrypeVulnerabilityMetadata
-        ).filter(
-            GrypeVulnerability.id == GrypeVulnerabilityMetadata.id
-        )
+            logger.debug("Querying grype_db for vuln_id: {}, namespace: {}, affected_package: {}".format(vuln_id, namespace, affected_package))
 
-        if vuln_id is not None:
-            query = query.filter(
-                GrypeVulnerability.id.in_(vuln_id)
-            )
-        if namespace is not None:
-            query = query.filter(
-                GrypeVulnerability.namespace.in_(namespace)
-            )
-        if affected_package is not None:
-            query = query.filter(
-                GrypeVulnerability.package_name == affected_package
+            query = grype_db_session.query(
+                GrypeVulnerability, GrypeVulnerabilityMetadata
+            # TODO Join on id explicitly between the two tables
+            ).filter(
+                GrypeVulnerability.id == GrypeVulnerabilityMetadata.id
             )
 
-        # TODO Query may/will need to be updated to return data that minimizes the downstream transformation cost
-        # The current return object data structure takes a kitchen sink approach, returning everything, and
-        # could be streamlined and/or optimized
-        return query.all()
+            if vuln_id is not None:
+                query = query.filter(
+                    GrypeVulnerability.id.in_(vuln_id)
+                )
+            if namespace is not None:
+                query = query.filter(
+                    GrypeVulnerability.namespace.in_(namespace)
+                )
+            if affected_package is not None:
+                query = query.filter(
+                    GrypeVulnerability.package_name == affected_package
+                )
+
+            # TODO Query may/will need to be updated to return data that minimizes the downstream transformation cost
+            # The current return object data structure takes a kitchen sink approach, returning everything, and
+            # could be streamlined and/or optimized
+            return query.all()
+        finally:
+            read_lock.release()
---
+++ tests/unit/anchore_engine/clients/test_grype_wrapper.py
@@ -0,0 +1,209 @@
+import anchore_engine.configuration.localconfig
+import os
+import pytest
+import shutil
+import sqlalchemy
+
+from anchore_engine.clients import grype_wrapper
+
+TEST_DATA_PATH = "../../data/grype_db/"
+GRYPE_DB_DIR = "grype_db/"
+
+
+@pytest.fixture
+def grype_db_dir(tmp_path):
+    localconfig = anchore_engine.configuration.localconfig.get_config()
+    localconfig["service_dir"] = tmp_path
+    anchore_engine.configuration.localconfig.localconfig = localconfig
+
+    return os.path.join(tmp_path, GRYPE_DB_DIR)
+
+
+@pytest.fixture
+def grype_db_archive(tmp_path):
+    input_dir = os.path.join(tmp_path, "input")
+    if not os.path.exists(input_dir):
+        os.mkdir(input_dir)
+    shutil.copy("../../data/grype_db/grype_db_test_archive.tar.gz", input_dir)
+    return os.path.join(input_dir, "grype_db_test_archive.tar.gz")
+
+
+@pytest.fixture
+def grype_db_file(tmp_path):
+    input_dir = os.path.join(tmp_path, "input")
+    if not os.path.exists(input_dir):
+        os.mkdir(input_dir)
+    shutil.copy("../../data/grype_db/vulnerability.db", input_dir)
+    return os.path.join(input_dir, "vulnerability.db")
+
+
+@pytest.fixture
+def old_grype_db_file(tmp_path):
+    input_dir = os.path.join(tmp_path, "input")
+    if not os.path.exists(input_dir):
+        os.mkdir(input_dir)
+    shutil.copy("../../data/grype_db/old_vulnerability.db", input_dir)
+    return os.path.join(input_dir, "vulnerability.db")
+
+
+def get_test_ubuntu_sbom(sbom_file_name):
+    full_sbom_path = os.path.join(TEST_DATA_PATH, sbom_file_name)
+    # with open(full_sbom_path, "r") as read_file:
+    #     return read_file.read().replace('\n', '')
+    return full_sbom_path
+
+
+def test_get_current_grype_db_checksum():
+    # Function under test
+    result = grype_wrapper.get_current_grype_db_checksum()
+
+    # Validate result
+    assert result == None
+
+
+def test_get_default_cache_dir_from_config(grype_db_dir, tmp_path):
+    # Function under test
+    local_db_dir = grype_wrapper._get_default_grype_db_dir_from_config()
+
+    # Validate the grype db dir exists and is in the correct location
+    assert os.path.exists(local_db_dir)
+    assert local_db_dir == grype_db_dir
+
+
+def test_move_grype_db_archive(tmp_path, grype_db_archive):
+    # Setup the output dir to be copied into
+    output_dir = os.path.join(tmp_path, "output")
+    os.mkdir(output_dir)
+
+    # Function under test
+    grype_db_archive_copied_file_location = grype_wrapper._move_grype_db_archive(grype_db_archive, output_dir)
+
+    # Validate archive was copied and to the correct location
+    assert os.path.exists(grype_db_archive_copied_file_location)
+    assert grype_db_archive_copied_file_location == os.path.join(output_dir, "grype_db_test_archive.tar.gz")
+
+
+def test_open_grype_db_archive(grype_db_archive):
+    # Setup expected output vars
+    parent_dir = os.path.abspath(os.path.join(grype_db_archive, os.pardir))
+    expected_grype_db_file = os.path.join(parent_dir, grype_wrapper.VULNERABILITY_FILE_NAME)
+
+    # Function under test
+    latest_grype_db_file = grype_wrapper._open_grype_db_archive(grype_db_archive, parent_dir)
+
+    # Validate expect files and their location
+    assert os.path.exists(expected_grype_db_file)
+    assert latest_grype_db_file == os.path.join(parent_dir, grype_wrapper.VULNERABILITY_FILE_NAME)
+
+
+def test_remove_grype_db_archive(grype_db_archive):
+    # Function under test
+    grype_wrapper._remove_grype_db_archive(grype_db_archive)
+
+    # Validate
+    assert not os.path.exists(grype_db_archive)
+
+
+def test_init_grype_db_engine(grype_db_file):
+    # Function under test
+    latest_grype_db_engine = grype_wrapper._init_grype_db_engine(grype_db_file)
+    assert str(latest_grype_db_engine.url) == "sqlite:///{}".format(grype_db_file)
+
+
+def test_init_grype_db_session(grype_db_file):
+    # Setup db engine
+    db_connect = "sqlite:///{}".format(grype_db_file)
+    latest_grype_db_engine = sqlalchemy.create_engine(db_connect, echo=True)
+    assert str(latest_grype_db_engine.url) == "sqlite:///{}".format(grype_db_file)
+
+    # Function under test
+    latest_grype_db_session = grype_wrapper._init_grype_db_session(latest_grype_db_engine)
+
+    # Validate output
+    assert latest_grype_db_session is not None
+
+
+def test_init_grype_db(grype_db_dir, grype_db_archive):
+    # Setup out vars
+    expected_output_file = os.path.join(grype_db_dir, grype_wrapper.VULNERABILITY_FILE_NAME)
+
+    # Function under test
+    latest_grype_db_file, latest_grype_db_session = grype_wrapper._init_grype_db(grype_db_archive)
+
+    assert latest_grype_db_file == expected_output_file
+    assert os.path.exists(latest_grype_db_file)
+    assert latest_grype_db_session is not None
+
+
+def test_remove_local_grype_db(grype_db_file):
+    # Function under test
+    grype_wrapper._remove_local_grype_db(grype_db_file)
+
+    # Validate output
+    assert not os.path.exists(grype_db_file)
+
+
+def test_update_grype_db(grype_db_dir, old_grype_db_file, grype_db_archive):
+    # Setup
+    grype_wrapper.grype_db_file = old_grype_db_file
+    expected_output_file = os.path.join(grype_db_dir, grype_wrapper.VULNERABILITY_FILE_NAME)
+
+    # Function under test
+    grype_wrapper.update_grype_db(grype_db_archive)
+
+    # Validate output
+    assert grype_wrapper.grype_db_file == expected_output_file
+    assert os.path.exists(grype_wrapper.grype_db_file)
+    assert grype_wrapper.grype_db_session is not None
+
+
+# TODO Disabling for now. Something in the test db file used here isn't playing nice with grype. It works ok for the query test below.
+# Googling it suggests the sqlite version used to read vs write the db can be a little touchy.
+# @pytest.mark.parametrize(
+#     "sbom_file_name, expected_output",
+#     [
+#         ("sbom-ubuntu-20.04--pruned.json", "ubuntu"),
+#     ],
+# )
+# def test_get_vulnerabilities(grype_db_file, sbom_file_name, expected_output):
+#     # Setup test inputs
+#     grype_wrapper.grype_db_file = grype_db_file
+#     test_sbom = get_test_ubuntu_sbom(sbom_file_name)
+#
+#     # Function under test
+#     result = grype_wrapper.get_vulnerabilities(test_sbom)
+#
+#     # TODO Assert expected results
+#     assert result["distro"]["name"] == expected_output
+
+
+@pytest.mark.parametrize(
+    "vuln_id, affected_package, namespace, expected_result_length, expected_output",
+    [
+        ("not_found", None, None, 0, []),
+        (None, "not_found", None, 0, []),
+        (None, None, "not_found", 0, []),
+        (None, None, None, 10, ["CVE-2019-16775", "CVE-2019-16777", "CVE-2019-16776", "CVE-2020-10174", "CVE-2019-2391", "CVE-2020-7610", "CVE-2020-8518", "CVE-2019-9658", "CVE-2019-15690", "CVE-2019-20788"]),
+        ("CVE-2019-16775", None, None, 1, ["CVE-2019-16775"]),
+        (None, "npm", None, 3, ["CVE-2019-16775", "CVE-2019-16777", "CVE-2019-16776"]),
+        (None, None, "debian:10", 10, ["CVE-2019-16775", "CVE-2019-16777", "CVE-2019-16776", "CVE-2020-10174", "CVE-2019-2391", "CVE-2020-7610", "CVE-2020-8518", "CVE-2019-9658", "CVE-2019-15690", "CVE-2019-20788"]),
+        ("CVE-2019-16775", "npm", "debian:10", 1, ["CVE-2019-16775"]),
+    ],
+)
+def test_query_vulnerabilities(
+        grype_db_file, vuln_id, affected_package, namespace, expected_result_length, expected_output
+):
+    # Setup the sqlalchemy artifacts on the test grype db file
+    test_grype_db_engine = grype_wrapper._init_grype_db_engine(grype_db_file)
+    grype_wrapper.grype_db_session = grype_wrapper._init_grype_db_session(test_grype_db_engine)
+
+
+    # Test and validate the query param combinations
+    results = grype_wrapper.query_vulnerabilities(
+        vuln_id=vuln_id,
+        affected_package=affected_package,
+        namespace=namespace,
+
+    )
+    assert len(results) == expected_result_length
+    assert list(map(lambda result: result[0].id, results)) == expected_output
