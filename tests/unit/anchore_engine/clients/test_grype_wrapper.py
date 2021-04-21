db7354f53af9f4899dad306d1aa8552c65a033ce
Dan Palmer <dan.palmer@anchore.com>
2021-04-21 16:21:01 -0400

    Add grype_wrapper facade (#977)
    * Add grype_wrapper with run_grype function and stubs.
    
    Signed-off-by: Dan Palmer <dan.palmer@anchore.com>
    
    * Add grype install to Dockerfile, first pass on returning vulnerabilites from grype db
    
    Signed-off-by: Dan Palmer <dan.palmer@anchore.com>
    
    * Add first pass on some locking logic for grype db reads and write.
    
    Signed-off-by: Dan Palmer <dan.palmer@anchore.com>
    
    * Refactor and add functionality and stubbed TODO methods.
    
    Signed-off-by: Dan Palmer <dan.palmer@anchore.com>
    
    * [ENTERPRISE-187] Refactor based on group discussion.
    
    Signed-off-by: Dan Palmer <dan.palmer@anchore.com>
    
    * [ENTERPRISE-187] Iterate on read/write lock, first pass on some unit tests for the same.
    
    Signed-off-by: Dan Palmer <dan.palmer@anchore.com>
    
    * Fix function names.
    
    Signed-off-by: Dan Palmer <dan.palmer@anchore.com>
    
    * Change out locking solution to something off the shelf. Refactor to expect grype db as a path to tarball. Add logging and some exception catching. Add multiple tests and test files
    
    Signed-off-by: Dan Palmer <dan.palmer@anchore.com>
    
    * Make function name more generic.
    
    Signed-off-by: Dan Palmer <dan.palmer@anchore.com>
    
    * Fix issue around dir vs file for cache env var on grype scan, add TODO to better handle that.
    
    Signed-off-by: Dan Palmer <dan.palmer@anchore.com>
    
    * Refine grype_db dir replacement logic, further improve tests.
    
    Signed-off-by: Dan Palmer <dan.palmer@anchore.com>
    
    * Fix pathing to test files in repo.
    
    Signed-off-by: Dan Palmer <dan.palmer@anchore.com>
    
    * Linting fixes.
    
    Signed-off-by: Dan Palmer <dan.palmer@anchore.com>
    
    * Remove draft locking logic in favor off-the-shelf.
    
    Signed-off-by: Dan Palmer <dan.palmer@anchore.com>
    
    * Additional linting fixes.
    
    Signed-off-by: Dan Palmer <dan.palmer@anchore.com>
    
    * Fix grype_db tables relationship and update query logic.
    
    Signed-off-by: Dan Palmer <dan.palmer@anchore.com>
    
    * Linting
    
    Signed-off-by: Dan Palmer <dan.palmer@anchore.com>
    
    * More linting
    
    Signed-off-by: Dan Palmer <dan.palmer@anchore.com>
    
    * Put back accidentally deleted file.
    
    Signed-off-by: Dan Palmer <dan.palmer@anchore.com>

---
+++ tests/unit/anchore_engine/clients/test_grype_wrapper.py
@@ -0,0 +1,296 @@
+import anchore_engine.configuration.localconfig
+import os
+import pytest
+import shutil
+import sqlalchemy
+
+from anchore_engine.clients import grype_wrapper
+
+
+TEST_DATA_RELATIVE_PATH = "../../data/grype_db/"
+GRYPE_ARCHIVE_FILE_NAME = "grype_db_test_archive.tar.gz"
+
+GRYPE_DB_DIR = "grype_db/"
+OLD_VERSION_NAME = "old_version"
+NEW_VERSION_NAME = "new_version"
+
+
+def get_test_file_path(basename: str) -> str:
+    return os.path.join(
+        os.path.dirname(os.path.abspath(__file__)), TEST_DATA_RELATIVE_PATH, basename
+    )
+
+
+def get_test_sbom(sbom_file_name):
+    # TODO Pass the body as a string, not the file
+    full_sbom_path = get_test_file_path(sbom_file_name)
+    # with open(full_sbom_path, "r") as read_file:
+    #     return read_file.read().replace('\n', '')
+    return full_sbom_path
+
+
+@pytest.fixture
+def grype_db_parent_dir(tmp_path):
+    localconfig = anchore_engine.configuration.localconfig.get_config()
+    localconfig["service_dir"] = tmp_path
+    anchore_engine.configuration.localconfig.localconfig = localconfig
+
+    return os.path.join(tmp_path, GRYPE_DB_DIR)
+
+
+@pytest.fixture
+def grype_db_archive(tmp_path):
+    input_dir = os.path.join(tmp_path, "input")
+    if not os.path.exists(input_dir):
+        os.mkdir(input_dir)
+    shutil.copy(get_test_file_path(GRYPE_ARCHIVE_FILE_NAME), input_dir)
+    return os.path.join(input_dir, "grype_db_test_archive.tar.gz")
+
+
+@pytest.fixture
+def grype_db_dir(tmp_path):
+    parent_dir = os.path.join(tmp_path, "input")
+    if not os.path.exists(parent_dir):
+        os.mkdir(parent_dir)
+    input_dir = os.path.join(parent_dir, NEW_VERSION_NAME)
+    shutil.copytree(get_test_file_path(NEW_VERSION_NAME), input_dir)
+    return input_dir
+
+
+@pytest.fixture
+def old_grype_db_dir(tmp_path):
+    parent_dir = os.path.join(tmp_path, "input")
+    if not os.path.exists(parent_dir):
+        os.mkdir(parent_dir)
+    input_dir = os.path.join(parent_dir, OLD_VERSION_NAME)
+    shutil.copytree(get_test_file_path(OLD_VERSION_NAME), input_dir)
+    return input_dir
+
+
+# TODO implement along with function under test
+# def test_get_current_grype_db_checksum():
+#     # Function under test
+#     result = grype_wrapper.get_current_grype_db_checksum()
+#
+#     # Validate result
+#     assert result == None
+
+
+def test_get_default_cache_dir_from_config(grype_db_parent_dir, tmp_path):
+    # Function under test
+    local_db_dir = grype_wrapper._get_default_grype_db_dir_from_config()
+
+    # Validate the grype db dir exists and is in the correct location
+    assert os.path.exists(local_db_dir)
+    assert local_db_dir == grype_db_parent_dir
+
+
+def test_move_grype_db_archive(tmp_path, grype_db_archive):
+    # Setup the output dir to be copied into
+    output_dir = os.path.join(tmp_path, "output")
+    os.mkdir(output_dir)
+
+    # Function under test
+    grype_db_archive_copied_file_location = grype_wrapper._move_grype_db_archive(
+        grype_db_archive, output_dir
+    )
+
+    # Validate archive was copied and to the correct location
+    assert os.path.exists(grype_db_archive_copied_file_location)
+    assert grype_db_archive_copied_file_location == os.path.join(
+        output_dir, "grype_db_test_archive.tar.gz"
+    )
+
+
+def test_open_grype_db_archive(grype_db_archive):
+    # Setup expected output vars
+    parent_dir = os.path.abspath(os.path.join(grype_db_archive, os.pardir))
+    expected_grype_db_file = os.path.join(parent_dir, NEW_VERSION_NAME)
+
+    # Function under test
+    latest_grype_db_dir = grype_wrapper._open_grype_db_archive(
+        grype_db_archive, parent_dir, NEW_VERSION_NAME
+    )
+
+    # Validate expected dir contents and location
+    assert os.path.exists(expected_grype_db_file)
+    assert latest_grype_db_dir == os.path.join(parent_dir, NEW_VERSION_NAME)
+    assert os.path.exists(
+        os.path.join(latest_grype_db_dir, grype_wrapper.VULNERABILITY_FILE_NAME)
+    )
+
+
+def test_remove_grype_db_archive(grype_db_archive):
+    # Function under test
+    grype_wrapper._remove_grype_db_archive(grype_db_archive)
+
+    # Validate
+    assert not os.path.exists(grype_db_archive)
+
+
+def test_init_grype_db_engine(grype_db_dir):
+    # Setup output var
+    vuln_file_path = os.path.join(grype_db_dir, grype_wrapper.VULNERABILITY_FILE_NAME)
+
+    # Function under test
+    latest_grype_db_engine = grype_wrapper._init_grype_db_engine(grype_db_dir)
+
+    # Validate expected output
+    assert str(latest_grype_db_engine.url) == "sqlite:///{}".format(vuln_file_path)
+
+
+def test_init_grype_db_session(grype_db_dir):
+    # Setup db engine
+    vuln_file_path = os.path.join(grype_db_dir, grype_wrapper.VULNERABILITY_FILE_NAME)
+    db_connect = "sqlite:///{}".format(vuln_file_path)
+    latest_grype_db_engine = sqlalchemy.create_engine(db_connect, echo=True)
+    assert str(latest_grype_db_engine.url) == "sqlite:///{}".format(vuln_file_path)
+
+    # Function under test
+    latest_grype_db_session = grype_wrapper._init_grype_db_session(
+        latest_grype_db_engine
+    )
+
+    # Validate output
+    assert latest_grype_db_session is not None
+
+
+def test_init_grype_db(grype_db_parent_dir, grype_db_archive):
+    # Setup out vars
+    expected_output_dir = os.path.join(grype_db_parent_dir, NEW_VERSION_NAME)
+    expected_output_file = os.path.join(
+        expected_output_dir, grype_wrapper.VULNERABILITY_FILE_NAME
+    )
+
+    # Function under test
+    latest_grype_db_dir, latest_grype_db_session = grype_wrapper._init_grype_db(
+        grype_db_archive, NEW_VERSION_NAME
+    )
+
+    # Validate expected output
+    assert os.path.exists(latest_grype_db_dir)
+    assert latest_grype_db_dir == expected_output_dir
+
+    assert latest_grype_db_session is not None
+    assert os.path.exists(expected_output_file)
+
+
+def test_remove_local_grype_db(old_grype_db_dir):
+    # Function under test
+    grype_wrapper._remove_local_grype_db(old_grype_db_dir)
+
+    # Validate output
+    assert not os.path.exists(old_grype_db_dir)
+
+
+def test_update_grype_db(grype_db_parent_dir, old_grype_db_dir, grype_db_archive):
+    # Setup
+    grype_wrapper.grype_db_dir = old_grype_db_dir
+    expected_output_dir = os.path.join(grype_db_parent_dir, NEW_VERSION_NAME)
+    expected_output_file = os.path.join(
+        expected_output_dir, grype_wrapper.VULNERABILITY_FILE_NAME
+    )
+
+    # Function under test
+    grype_wrapper.update_grype_db(grype_db_archive, NEW_VERSION_NAME)
+
+    # Validate output
+    assert os.path.exists(grype_wrapper.grype_db_dir)
+    assert grype_wrapper.grype_db_dir == expected_output_dir
+
+    assert grype_wrapper.grype_db_session is not None
+    assert os.path.exists(expected_output_file)
+
+    assert not os.path.exists(old_grype_db_dir)
+
+
+# TODO This needs to move to a functional test to pass on the ci, as the container with grype
+# is not built before that. Disabling for now. Works locally if you have grype installed.
+# @pytest.mark.parametrize(
+#     "sbom_file_name, expected_output",
+#     [("sbom-ubuntu-20.04--pruned.json", "ubuntu")],
+# )
+# def test_get_vulnerabilities(grype_db_dir, sbom_file_name, expected_output):
+#     # Setup test inputs
+#     grype_wrapper.grype_db_dir = grype_db_dir
+#     test_sbom = get_test_sbom(sbom_file_name)
+#
+#     # Function under test
+#     result = grype_wrapper.get_vulnerabilities(test_sbom)
+#
+#     # TODO Assert expected results
+#     assert result["distro"]["name"] == expected_output
+
+
+@pytest.mark.parametrize(
+    "vuln_id, affected_package, namespace, expected_result_length, expected_output",
+    [
+        ("not_found", None, None, 0, []),
+        (None, "not_found", None, 0, []),
+        (None, None, "not_found", 0, []),
+        (
+            None,
+            None,
+            None,
+            10,
+            [
+                "CVE-2019-16775",
+                "CVE-2019-16777",
+                "CVE-2019-16776",
+                "CVE-2020-10174",
+                "CVE-2019-2391",
+                "CVE-2020-7610",
+                "CVE-2020-8518",
+                "CVE-2019-9658",
+                "CVE-2019-15690",
+                "CVE-2019-20788",
+            ],
+        ),
+        ("CVE-2019-16775", None, None, 1, ["CVE-2019-16775"]),
+        (None, "npm", None, 3, ["CVE-2019-16775", "CVE-2019-16777", "CVE-2019-16776"]),
+        (
+            None,
+            None,
+            "debian:10",
+            10,
+            [
+                "CVE-2019-16775",
+                "CVE-2019-16777",
+                "CVE-2019-16776",
+                "CVE-2020-10174",
+                "CVE-2019-2391",
+                "CVE-2020-7610",
+                "CVE-2020-8518",
+                "CVE-2019-9658",
+                "CVE-2019-15690",
+                "CVE-2019-20788",
+            ],
+        ),
+        ("CVE-2019-16775", "npm", "debian:10", 1, ["CVE-2019-16775"]),
+    ],
+)
+def test_query_vulnerabilities(
+    grype_db_dir,
+    vuln_id,
+    affected_package,
+    namespace,
+    expected_result_length,
+    expected_output,
+):
+    # Setup the sqlalchemy artifacts on the test grype db
+    test_grype_db_engine = grype_wrapper._init_grype_db_engine(grype_db_dir)
+    grype_wrapper.grype_db_session = grype_wrapper._init_grype_db_session(
+        test_grype_db_engine
+    )
+
+    # Test and validate the query param combinations
+    results = grype_wrapper.query_vulnerabilities(
+        vuln_id=vuln_id,
+        affected_package=affected_package,
+        namespace=namespace,
+    )
+    assert len(results) == expected_result_length
+    assert list(map(lambda result: result.id, results)) == expected_output
+    # TODO Assert joined vulnerability_metadata is correct
+    # I need to further simplify the test data set to keep the expected_output size manageable
+    # Or else that matrix is just going to be unreadable
