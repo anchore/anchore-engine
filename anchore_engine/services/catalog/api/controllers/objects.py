92319cd1a79031b22785d440727610061093120a
Vijay Pillai <vijay.pillai@anchore.com>
2021-04-26 12:37:10 -0400

    add internal endpoint to catalog service for posting binary content to object store
    Signed-off-by: Vijay Pillai <vijay.pillai@anchore.com>
    Signed-off-by: Zane Burstein <zane.burstein@anchore.com>
    
    Authored-by: Zane Burstein <zane.burstein@anchore.com>

--- anchore_engine/services/catalog/api/controllers/objects.py
+++ anchore_engine/services/catalog/api/controllers/objects.py
@@ -6,10 +6,10 @@ Simple get/post/delete api for unstructured data and json data storage
 """
 
 import json
+
 import anchore_engine.apis
 import anchore_engine.services
 import anchore_engine.subsys.object_store.manager
-
 from anchore_engine import utils as anchore_utils
 from anchore_engine.apis.authorization import INTERNAL_SERVICE_ALLOWED, get_authorizer
 from anchore_engine.apis.context import ApiRequestContextProxy
@@ -22,60 +22,17 @@ authorizer = get_authorizer()
 @flask_metrics.do_not_track()
 @authorizer.requires_account(with_types=INTERNAL_SERVICE_ALLOWED)
 def get_object(bucket, archiveid):
-    httpcode = 500
-    try:
-        obj_mgr = anchore_engine.subsys.object_store.manager.get_manager()
-        account_name = ApiRequestContextProxy.namespace()
-        try:
-            return_object = json.loads(
-                anchore_utils.ensure_str(obj_mgr.get(account_name, bucket, archiveid))
-            )
-            httpcode = 200
-        except Exception as err:
-            httpcode = 404
-            raise err
-    except Exception as err:
-        return_object = anchore_engine.common.helpers.make_response_error(
-            err, in_httpcode=httpcode
-        )
-
+    return_object, httpcode = get_object_from_storage(bucket, archiveid, is_json=True)
     return return_object, httpcode
 
 
 @flask_metrics.do_not_track()
 @authorizer.requires_account(with_types=INTERNAL_SERVICE_ALLOWED)
 def create_object(bucket, archiveid, bodycontent):
-    httpcode = 500
-    try:
-        account_name = ApiRequestContextProxy.namespace()
-        obj_mgr = anchore_engine.subsys.object_store.manager.get_manager()
-
-        jsonbytes = anchore_utils.ensure_bytes(json.dumps(bodycontent))
-        rc = obj_mgr.put(account_name, bucket, archiveid, jsonbytes)
-
-        my_svc = ApiRequestContextProxy.get_service()
-        if my_svc is not None:
-            resource_url = (
-                my_svc.service_record["base_url"]
-                + "/"
-                + my_svc.service_record["version"]
-                + "/archive/"
-                + bucket
-                + "/"
-                + archiveid
-            )
-        else:
-            resource_url = "N/A"
-
-        return_object = resource_url
-        httpcode = 200
-
-    except Exception as err:
-        return_object = anchore_engine.common.helpers.make_response_error(
-            err, in_httpcode=httpcode
-        )
-
-    return return_object, httpcode
+    return_object, http_code = create_object_in_storage(
+        bucket, archiveid, bodycontent, is_json=True
+    )
+    return return_object, http_code
 
 
 @flask_metrics.do_not_track()
@@ -94,3 +51,93 @@ def delete_object(bucket, archiveid):
         )
 
     return return_object, httpcode
+
+
+@flask_metrics.do_not_track()
+@authorizer.requires_account(with_types=INTERNAL_SERVICE_ALLOWED)
+def create_raw_object(bucket, archiveid, bodycontent):
+    return_object, http_code = create_object_in_storage(
+        bucket, archiveid, bodycontent, is_json=False
+    )
+    return return_object, http_code
+
+
+@flask_metrics.do_not_track()
+@authorizer.requires_account(with_types=INTERNAL_SERVICE_ALLOWED)
+def get_raw_object(bucket, archiveid):
+    return_object, httpcode = get_object_from_storage(bucket, archiveid, is_json=False)
+    return return_object, httpcode
+
+
+def create_object_in_storage(
+    bucket: str, archiveid: str, bodycontent: str, is_json: bool = False
+):
+    """
+    Creates an object in storeage with the object storage manager.
+    Takes param to determine if it should be stored as json or not.
+    Used by two endpoints so that a single endpoint does not return multiple different content types
+    """
+    http_code = 500
+    try:
+        account_name = ApiRequestContextProxy.namespace()
+        obj_mgr = anchore_engine.subsys.object_store.manager.get_manager()
+
+        if is_json:
+            bodycontent = anchore_utils.ensure_bytes(json.dumps(bodycontent))
+
+        obj_mgr.put(account_name, bucket, archiveid, bodycontent)
+    except Exception as err:
+        return_object = anchore_engine.common.helpers.make_response_error(
+            err, in_httpcode=http_code
+        )
+        return return_object, http_code
+
+    my_svc = ApiRequestContextProxy.get_service()
+    if my_svc is None:
+        resource_url = "N/A"
+    else:
+        try:
+            path_parts = [
+                my_svc.service_record["base_url"],
+                my_svc.service_record["version"],
+                "archive",
+                bucket,
+                archiveid,
+            ]
+        except KeyError as err:
+            return_object = anchore_engine.common.helpers.make_response_error(
+                err, in_httpcode=http_code
+            )
+            return return_object, http_code
+        resource_url = "/".join(path_parts)
+    return_object = resource_url
+    http_code = 200
+    return return_object, http_code
+
+
+def get_object_from_storage(bucket: str, archiveid: str, is_json: bool = False):
+    """
+    Gets an object from object storage
+    Has condition to return it as json or not
+    This is used by two endpoints so that a single endpoint does not return multiple different content types
+    """
+    http_code = 200
+    try:
+        obj_mgr = anchore_engine.subsys.object_store.manager.get_manager()
+        account_name = ApiRequestContextProxy.namespace()
+        return_object = obj_mgr.get(account_name, bucket, archiveid)
+
+        if not return_object:
+            http_code = 404
+            return_object = anchore_engine.common.helpers.make_response_error(
+                "No document found at given path", in_httpcode=http_code
+            )
+        elif is_json:
+            return_object = json.loads(return_object)
+
+    except Exception as err:
+        http_code = 500
+        return_object = anchore_engine.common.helpers.make_response_error(
+            err, in_httpcode=http_code
+        )
+    return return_object, http_code
