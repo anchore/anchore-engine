fdd24b41c0d722768eaf31f1a343e8ca75d6f7f9
Vijay Pillai <vijay.pillai@anchore.com>
2021-07-28 16:46:12 -0400

    correct install path of certifi in docker-entrypoint.sh
    Signed-off-by: Vijay Pillai <vijay.pillai@anchore.com>

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -10,7 +10,7 @@ if [[ -d "/home/anchore/certs" ]] && [[ ! -z "$(ls -A /home/anchore/certs)" ]]; 
     mkdir -p /home/anchore/certs_override/python
     mkdir -p /home/anchore/certs_override/os
     ### for python
-    cp /usr/local/lib/python3.6/site-packages/certifi/cacert.pem /home/anchore/certs_override/python/cacert.pem
+    cp /usr/local/lib/python3.8/site-packages/certifi/cacert.pem /home/anchore/certs_override/python/cacert.pem
     for file in /home/anchore/certs/*; do
         if grep -q 'BEGIN CERTIFICATE' "${file}"; then
             cat "${file}" >> /home/anchore/certs_override/python/cacert.pem
