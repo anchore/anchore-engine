5608b13434d71d03ee978d7673400816e9267bf1
Swathi Gangisetty <swathi@anchore.com>
2021-09-19 22:41:20 -0700

    Grype provider sbom generation update.
    - Switch the content source for sbom generation in grype provider from policy engine persistence to catalog image content API. The catalog API is also used to serve user image content requests. This change ensures that all content related requests operate over the same source data and it improves result coherenence and transperency
    - Add unit tests for image-content to sbom mappers
    
    Signed-off-by: Swathi Gangisetty <swathi@anchore.com>

--- tests/unit/anchore_engine/services/policy_engine/engine/vulns/test_mappers.py
+++ tests/unit/anchore_engine/services/policy_engine/engine/vulns/test_mappers.py
@@ -59,3 +59,182 @@ def test_engine_package_mappers(test_type, expected_type):
 def test_grype_package_mappers(test_type, expected_type):
     mapper = GRYPE_PACKAGE_MAPPERS.get(test_type)
     assert mapper.engine_type == expected_type
+
+
+class TestImageContentAPIToGrypeSbom:
+    @pytest.mark.parametrize(
+        "mapper, test_input, expected",
+        [
+            pytest.param(
+                ENGINE_PACKAGE_MAPPERS.get("npm"),
+                {
+                    "cpes": [
+                        "cpe:2.3:a:lodash:lodash:4.17.4:*:*:*:*:*:*:*",
+                        "cpe:2.3:a:*:lodash:4.17.4:*:*:*:*:*:*:*",
+                    ],
+                    "license": "MIT",
+                    "licenses": ["MIT"],
+                    "location": "/node_modules/lodash/package.json",
+                    "origin": "John-David Dalton <john.david.dalton@gmail.com> (http://allyoucanleet.com/)",
+                    "package": "lodash",
+                    "type": "NPM",
+                    "version": "4.17.4",
+                },
+                {
+                    "name": "lodash",
+                    "type": "npm",
+                    "language": "javascript",
+                    "locations": [{"path": "/node_modules/lodash/package.json"}],
+                    "cpes": [
+                        "cpe:2.3:a:*:lodash:4.17.4:*:*:*:*:*:*:*",
+                        "cpe:2.3:a:lodash:lodash:4.17.4:*:*:*:*:*:*:*",
+                    ],
+                    "version": "4.17.4",
+                },
+                id="npm",
+            ),
+            pytest.param(
+                ENGINE_PACKAGE_MAPPERS.get("gem"),
+                {
+                    "cpes": [
+                        "cpe:2.3:a:jessica-lynn-suttles:bundler:2.1.4:*:*:*:*:*:*:*",
+                        "cpe:2.3:a:jessica_lynn_suttles:bundler:2.1.4:*:*:*:*:*:*:*",
+                    ],
+                    "license": "MIT",
+                    "licenses": ["MIT"],
+                    "location": "/usr/lib/ruby/gems/2.7.0/specifications/bundler-2.1.4.gemspec",
+                    "origin": "...",
+                    "package": "bundler",
+                    "type": "GEM",
+                    "version": "2.1.4",
+                },
+                {
+                    "name": "bundler",
+                    "type": "gem",
+                    "language": "ruby",
+                    "locations": [
+                        {
+                            "path": "/usr/lib/ruby/gems/2.7.0/specifications/bundler-2.1.4.gemspec"
+                        }
+                    ],
+                    "cpes": [
+                        "cpe:2.3:a:jessica_lynn_suttles:bundler:2.1.4:*:*:*:*:*:*:*",
+                        "cpe:2.3:a:jessica-lynn-suttles:bundler:2.1.4:*:*:*:*:*:*:*",
+                    ],
+                    "version": "2.1.4",
+                },
+                id="gem",
+            ),
+            pytest.param(
+                ENGINE_PACKAGE_MAPPERS.get("python"),
+                {
+                    "cpes": [
+                        "cpe:2.3:a:python-pip:pip:21.2.2:*:*:*:*:*:*:*",
+                        "cpe:2.3:a:python:pip:21.2.2:*:*:*:*:*:*:*",
+                        "cpe:2.3:a:pip:pip:21.2.2:*:*:*:*:*:*:*",
+                    ],
+                    "license": "MIT",
+                    "licenses": ["MIT"],
+                    "location": "/usr/local/lib/python3.9/site-packages/pip",
+                    "origin": "The pip developers <distutils-sig@python.org>",
+                    "package": "pip",
+                    "type": "PYTHON",
+                    "version": "21.2.2",
+                },
+                {
+                    "name": "pip",
+                    "version": "21.2.2",
+                    "type": "python",
+                    "cpes": [
+                        "cpe:2.3:a:python-pip:pip:21.2.2:*:*:*:*:*:*:*",
+                        "cpe:2.3:a:python:pip:21.2.2:*:*:*:*:*:*:*",
+                        "cpe:2.3:a:pip:pip:21.2.2:*:*:*:*:*:*:*",
+                    ],
+                    "language": "python",
+                    "locations": [
+                        {"path": "/usr/local/lib/python3.9/site-packages/pip"}
+                    ],
+                },
+                id="python",
+            ),
+            pytest.param(
+                ENGINE_PACKAGE_MAPPERS.get("dpkg"),
+                {
+                    "cpes": ["cpe:2.3:a:bsdutils:bsdutils:1:2.33.1-0.1:*:*:*:*:*:*:*"],
+                    "license": "BSD-2-clause BSD-3-clause BSD-4-clause GPL-2 GPL-2+ GPL-3 GPL-3+ LGPL LGPL-2 LGPL-2+ LGPL-2.1 LGPL-2.1+ LGPL-3 LGPL-3+ MIT public-domain",
+                    "licenses": [
+                        "BSD-2-clause",
+                    ],
+                    "origin": "LaMont Jones <lamont@debian.org> (maintainer)",
+                    "package": "bsdutils",
+                    "size": "293000",
+                    "sourcepkg": "util-linux",
+                    "type": "dpkg",
+                    "version": "1:2.33.1-0.1",
+                },
+                {
+                    "name": "bsdutils",
+                    "version": "1:2.33.1-0.1",
+                    "type": "deb",
+                    "cpes": ["cpe:2.3:a:bsdutils:bsdutils:1:2.33.1-0.1:*:*:*:*:*:*:*"],
+                    "locations": [{"path": "pkgdb"}],
+                    "metadataType": "DpkgMetadata",
+                    "metadata": {"source": "util-linux"},
+                },
+                id="dpkg-with-source",
+            ),
+            pytest.param(
+                ENGINE_PACKAGE_MAPPERS.get("APKG"),
+                {
+                    "cpes": [
+                        "cpe:2.3:a:ssl-client:ssl_client:1.32.1-r5:*:*:*:*:*:*:*",
+                        "cpe:2.3:a:ssl_client:ssl_client:1.32.1-r5:*:*:*:*:*:*:*",
+                        "cpe:2.3:a:ssl-client:ssl-client:1.32.1-r5:*:*:*:*:*:*:*",
+                        "cpe:2.3:a:ssl_client:ssl-client:1.32.1-r5:*:*:*:*:*:*:*",
+                        "cpe:2.3:a:ssl:ssl_client:1.32.1-r5:*:*:*:*:*:*:*",
+                        "cpe:2.3:a:ssl:ssl-client:1.32.1-r5:*:*:*:*:*:*:*",
+                    ],
+                    "license": "GPL-2.0-only",
+                    "licenses": ["GPL-2.0-only"],
+                    "origin": "Natanael Copa <ncopa@alpinelinux.org>",
+                    "package": "ssl_client",
+                    "size": "28672",
+                    "sourcepkg": "busybox",
+                    "type": "APKG",
+                    "version": "1.32.1-r5",
+                },
+                {
+                    "name": "ssl_client",
+                    "version": "1.32.1-r5",
+                    "type": "apk",
+                    "cpes": [
+                        "cpe:2.3:a:ssl-client:ssl_client:1.32.1-r5:*:*:*:*:*:*:*",
+                        "cpe:2.3:a:ssl_client:ssl_client:1.32.1-r5:*:*:*:*:*:*:*",
+                        "cpe:2.3:a:ssl-client:ssl-client:1.32.1-r5:*:*:*:*:*:*:*",
+                        "cpe:2.3:a:ssl_client:ssl-client:1.32.1-r5:*:*:*:*:*:*:*",
+                        "cpe:2.3:a:ssl:ssl_client:1.32.1-r5:*:*:*:*:*:*:*",
+                        "cpe:2.3:a:ssl:ssl-client:1.32.1-r5:*:*:*:*:*:*:*",
+                    ],
+                    "locations": [{"path": "pkgdb"}],
+                    "metadataType": "ApkgMetadata",
+                    "metadata": {"originPackage": "busybox"},
+                },
+                id="apkg-with-source",
+            ),
+        ],
+    )
+    def test_mappers(self, mapper, test_input, expected):
+        actual = mapper.image_content_to_grype_sbom(test_input)
+
+        # sort the list attributes before comparing
+        actual = {
+            key: sorted(value) if isinstance(value, list) else value
+            for key, value in actual.items()
+        }
+        expected = {
+            key: sorted(value) if isinstance(value, list) else value
+            for key, value in expected.items()
+        }
+
+        assert actual.pop("id")
+        assert actual == expected
