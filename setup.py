62e8875b8ae7f4bc67c79fd676523d3a5399119d
Vijay Pillai <vijay.pillai@anchore.com>
2021-10-18 10:06:20 -0400

    clean up setup file, move package_data to MANIFEST.in
    Signed-off-by: Vijay Pillai <vijay.pillai@anchore.com>

--- setup.py
+++ setup.py
@@ -6,53 +6,25 @@ from setuptools import find_packages, setup
 
 from anchore_engine import version
 
-version = version.version
 package_name = "anchore_engine"
-description = "Anchore Engine"
-long_description = open("README.md").read()
-url = "http://www.anchore.com"
 
 with open("requirements.txt") as f:
     requirements = f.read().splitlines()
 
-# find all the swaggers
-swaggers = []
-for root, dirnames, filenames in os.walk("./" + package_name):
-    if "swagger.yaml" in filenames:
-        theswaggerdir = re.sub(re.escape("./" + package_name + "/"), "", root)
-        swaggers.append("/".join([theswaggerdir, "swagger.yaml"]))
-
-package_data = {
-    package_name: [
-        "conf/*",
-        "conf/bundles/*",
-        "analyzers/modules/*",
-    ]
-    + swaggers,
-    "twisted": ["plugins/*"],
-}
-
-data_files = []
-# scripts = ['scripts/anchore-engine']
-scripts = []
-packages = find_packages(exclude=["test", "test.*"])
-packages.append("twisted.plugins")
 setup(
     name="anchore_engine",
     author="Anchore Inc.",
     author_email="dev@anchore.com",
     license="Apache License 2.0",
-    description=description,
-    long_description=long_description,
-    url=url,
+    description="Anchore Engine",
+    long_description=open("README.md").read(),
+    url="http://www.anchore.com",
     python_requires="==3.8.*",
-    packages=packages,
-    version=version,
-    data_files=data_files,
+    packages=find_packages(exclude=["test", "test.*"]) + ["twisted.plugins"],
+    version=version.version,
     include_package_data=True,
-    package_data=package_data,
     install_requires=requirements,
-    scripts=scripts,
+    scripts=[],
     entry_points="""
     [console_scripts]
     anchore-manager=anchore_manager.cli:main_entry
