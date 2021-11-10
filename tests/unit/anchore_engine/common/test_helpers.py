1ad5aeaeb59f7cba2e298d7ccf97335cfe54c8b6
Samuel Dacanay <sam.dacanay@anchore.com>
2021-11-10 16:13:17 +0200

    Add unit test for make_response_error
    Signed-off-by: Samuel Dacanay <sam.dacanay@anchore.com>

--- tests/unit/anchore_engine/common/test_helpers.py
+++ tests/unit/anchore_engine/common/test_helpers.py
@@ -45,3 +45,164 @@ class TestExtractPythonContent:
         assert len(extracted_content[key]["licenses"]) == 1
         assert extracted_content[key]["licenses"][0] == "MIT"
         assert len(extracted_content[key]["cpes"]) > 0
+
+
+class TestMakeResponseError:
+    class TestException(Exception):
+        def __init__(self, msg, anchore_error_json=None):
+            super().__init__(msg)
+            if anchore_error_json is not None:
+                self.anchore_error_json = anchore_error_json
+
+    params = [
+        pytest.param(
+            {
+                "errmsg": "basic-test-case",
+                "in_httpcode": None,
+                "details": None,
+                "expected": {
+                    "message": "basic-test-case",
+                    "httpcode": 500,
+                    "detail": {"error_codes": []},
+                },
+            },
+            id="basic",
+        ),
+        pytest.param(
+            {
+                "errmsg": "basic-test-case",
+                "in_httpcode": 400,
+                "details": None,
+                "expected": {
+                    "message": "basic-test-case",
+                    "httpcode": 400,
+                    "detail": {"error_codes": []},
+                },
+            },
+            id="basic-with-httpcode",
+        ),
+        pytest.param(
+            {
+                "errmsg": "basic-test-case",
+                "in_httpcode": None,
+                "details": {"test": "value"},
+                "expected": {
+                    "message": "basic-test-case",
+                    "httpcode": 500,
+                    "detail": {
+                        "test": "value",
+                        "error_codes": [],
+                    },
+                },
+            },
+            id="basic-with-details",
+        ),
+        pytest.param(
+            {
+                "errmsg": "basic-test-case",
+                "in_httpcode": None,
+                "details": {"error_codes": [500, 404]},
+                "expected": {
+                    "message": "basic-test-case",
+                    "httpcode": 500,
+                    "detail": {"error_codes": [500, 404]},
+                },
+            },
+            id="basic-with-error-codes",
+        ),
+        pytest.param(
+            {
+                "errmsg": Exception("thisisatest"),
+                "in_httpcode": None,
+                "details": None,
+                "expected": {
+                    "message": "thisisatest",
+                    "httpcode": 500,
+                    "detail": {"error_codes": []},
+                },
+            },
+            id="basic-exception",
+        ),
+        pytest.param(
+            {
+                "errmsg": TestException(
+                    "testexception",
+                    anchore_error_json={
+                        "message": "test",
+                        "httpcode": 500,
+                        "detail": {"error_codes": [404]},
+                    },
+                ),
+                "in_httpcode": 400,
+                "details": None,
+                "expected": {
+                    "message": "test",
+                    "httpcode": 500,
+                    "detail": {"error_codes": [404]},
+                },
+            },
+            id="basic-exception-with-anchore-error-json",
+        ),
+        pytest.param(
+            {
+                "errmsg": TestException(
+                    "testexception",
+                    anchore_error_json={
+                        "message": "test",
+                        "httpcode": 500,
+                        "detail": {"error_codes": [404]},
+                        "error_code": 401,
+                    },
+                ),
+                "in_httpcode": 400,
+                "details": None,
+                "expected": {
+                    "message": "test",
+                    "httpcode": 500,
+                    "detail": {"error_codes": [404, 401]},
+                },
+            },
+            id="basic-exception-with-anchore-error-json-and-error-code",
+        ),
+        pytest.param(
+            {
+                "errmsg": TestException(
+                    "testexception",
+                    anchore_error_json='{"message": "test", "httpcode": 500, "detail": {"error_codes": [404]}}',
+                ),
+                "in_httpcode": 400,
+                "details": None,
+                "expected": {
+                    "message": "test",
+                    "httpcode": 500,
+                    "detail": {"error_codes": [404]},
+                },
+            },
+            id="basic-exception-with-json-string",
+        ),
+        pytest.param(
+            {
+                "errmsg": TestException(
+                    "testexception",
+                    anchore_error_json='{"message" "test", "httpcode": 500, "detail": {"error_codes": [404]}}',
+                ),
+                "in_httpcode": 400,
+                "details": None,
+                "expected": {
+                    "message": "testexception",
+                    "httpcode": 400,
+                    "detail": {"error_codes": []},
+                },
+            },
+            id="basic-exception-with-bad-json-string",
+        ),
+    ]
+
+    @pytest.mark.parametrize("param", params)
+    def test_make_response_error(self, param):
+        actual = helpers.make_response_error(
+            param["errmsg"], param["in_httpcode"], param["details"]
+        )
+        assert actual["message"] == param["expected"]["message"]
+        assert actual["httpcode"] == param["expected"]["httpcode"]
+        assert actual["detail"] == param["expected"]["detail"]
