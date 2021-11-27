78a414ab7a64c72b78924ac2851abba304685894
Samuel Dacanay <sam.dacanay@anchore.com>
2021-11-27 09:31:18 +0200

    Add unit test for syft handlers save_entry
    Signed-off-by: Samuel Dacanay <sam.dacanay@anchore.com>

---
+++ tests/unit/anchore_engine/analyzers/syft/handlers/test_alpine.py
@@ -0,0 +1,42 @@
+import pytest
+
+from anchore_engine.analyzers.syft.handlers.alpine import save_entry
+
+
+class TestAlpine:
+    @pytest.mark.parametrize(
+        "param",
+        [
+            pytest.param(
+                {
+                    "findings": {},
+                    "engine_entry": {"name": "test"},
+                    "pkg_key": "basic-test",
+                    "expected": {"name": "test"},
+                    "expected_key": "basic-test",
+                },
+                id="basic-case",
+            ),
+            pytest.param(
+                {
+                    "findings": {},
+                    "engine_entry": {"name": "test"},
+                    "pkg_key": None,
+                    "expected": {"name": "test"},
+                    "expected_key": "test",
+                },
+                id="no-pkgkey-case",
+            ),
+        ],
+    )
+    def test_save_entry(self, param):
+        findings = param["findings"]
+        save_entry(findings, param["engine_entry"], param["pkg_key"])
+        findings_key = param["expected_key"]
+        assert (
+            findings.get("package_list", {})
+            .get("pkgs.allinfo", {})
+            .get("base", {})
+            .get(findings_key, {})
+            == param["expected"]
+        )
