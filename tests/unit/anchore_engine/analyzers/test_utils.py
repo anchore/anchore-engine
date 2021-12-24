7dfd7ef974edef7dc0a74c86472e142ac8cfdbda
Samuel Dacanay <sam.dacanay@anchore.com>
2021-12-24 11:36:52 -0300

    Add unit test for the write_kvfile_fomdict method
    Signed-off-by: Samuel Dacanay <sam.dacanay@anchore.com>

--- tests/unit/anchore_engine/analyzers/test_utils.py
+++ tests/unit/anchore_engine/analyzers/test_utils.py
@@ -1,6 +1,8 @@
+import os
+
 import pytest
 
-from anchore_engine.analyzers.utils import dig
+from anchore_engine.analyzers.utils import dig, write_kvfile_fromdict
 
 
 class TestDig:
@@ -55,3 +57,84 @@ class TestDig:
 
     def test_none_empty_value_with_force_default(self):
         assert dig({"a": "b!"}, "a", force_default=12) == "b!"
+
+
+# This fixture runs to include the teardown of the file that is created as part of the
+# tested method's normal execution. It also provides the parameters to the unit test below
+@pytest.fixture(
+    params=[
+        pytest.param(
+            {
+                "filename": "./basic-file-test.json",
+                "dict": {"key": "value"},
+                "expected": "key value \n",
+            },
+            id="basic-file",
+        ),
+        pytest.param(
+            {
+                "filename": "./null-value-test.json",
+                "dict": {"key": ""},
+                "expected": "key none \n",
+            },
+            id="null-value",
+        ),
+        pytest.param(
+            {
+                "filename": "./nonstring-key-test.json",
+                "dict": {2: ""},
+                "expected": TypeError(
+                    "Expected value of key 2 to be a string, found int"
+                ),
+            },
+            id="nonstring-key",
+        ),
+        pytest.param(
+            {
+                "filename": "./key-with-spaces-test.json",
+                "dict": {"space key": "value"},
+                "expected": "space____key value \n",
+            },
+            id="key-with-spaces",
+        ),
+        pytest.param(
+            {
+                "filename": "./multiple-keys-test.json",
+                "dict": {
+                    "key1": "value1",
+                    "key2": "value2",
+                },
+                "expected": "key1 value1 \nkey2 value2 \n",
+            },
+            id="multiple-keys",
+        ),
+    ]
+)
+def kvfile_fixture(request):
+    filename = request.param.get("filename", None)
+    if filename is not None:
+
+        def delete_file():
+            os.remove(filename)
+
+        request.addfinalizer(delete_file)
+    else:
+        raise Exception("can't get filename")
+
+    return request.param
+
+
+def test_write_kvfile_fromdict(kvfile_fixture):
+    param = kvfile_fixture
+    if isinstance(param["expected"], TypeError):
+        with pytest.raises(TypeError) as actualerr:
+            write_kvfile_fromdict(param["filename"], param["dict"])
+            assert actualerr == param["expected"]
+            with open(param["filename"], "r") as f:
+                actual = f.read()
+                assert actual == ""
+    else:
+        write_kvfile_fromdict(param["filename"], param["dict"])
+        with open(param["filename"], "r") as f:
+            actual = f.read()
+            assert actual == param["expected"]
